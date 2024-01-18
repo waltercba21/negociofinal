c1fac7e61271fef2206324ab062a16b40aa02de3
Walter Cordoba <waltercordobadev@gmail.com>
2024-01-18 08:46:37 -0300

    cambios

--- config/conexion.js
+++ config/conexion.js
@@ -1,19 +1,18 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'productos'
-})
+    host: '127.0.0.1', 
+    port:'3306',
+    user: 'walter',
+    password: '123456',
+    database: 'autofaros'
+});
 
-conexion.connect (
-    (error) => {
-        if (!error) {
-            console.log('Conexion establecida a la base de datos')
-        }else{
-            console.log('Error de conexion');
-        }
+conexion.connect((error) => {
+    if (!error) {
+        console.log('Conexion establecida a la base de datos');
+    } else {
+        console.error('Error de conexion:', error);
     }
-);
+});
 
 module.exports = conexion;
