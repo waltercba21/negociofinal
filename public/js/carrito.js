64e88337522ca00b058a4683e8b35f3fd9de0a1e
Walter Cordoba <waltercordobadev@gmail.com>
2024-02-19 13:46:42 -0300

    arreglado carrito de compras

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,5 +1,87 @@
-console.log('El script se está cargando');
 
+function mas(id) {
+  var inputCantidad = document.querySelector('#cantidad-' + id);
+  var cantidad = parseInt(inputCantidad.value, 10);
+  cantidad = isNaN(cantidad) ? 0 : cantidad;
+  cantidad++;
+  inputCantidad.value = cantidad;
+
+  var cantidadTabla = document.querySelector('#cantidad-tabla-' + id);
+  cantidadTabla.textContent = cantidad;
+
+  actualizarCantidad(id);
+  actualizarTotal();
+}
+
+function menos(id) {
+  var inputCantidad = document.querySelector('#cantidad-' + id);
+  var cantidad = parseInt(inputCantidad.value, 10);
+  cantidad = isNaN(cantidad) ? 0 : cantidad;
+  if (cantidad > 0) {
+      cantidad--;
+  }
+  inputCantidad.value = cantidad;
+
+  var cantidadTabla = document.querySelector('#cantidad-tabla-' + id);
+  cantidadTabla.textContent = cantidad;
+
+  actualizarCantidad(id);
+  actualizarTotal();
+}
+
+    function actualizarTotal() {
+    var totalCantidad = 0;
+    var totalPrecio = 0;
+  
+    // Obtén todos los productos en el carrito
+    var productos = document.querySelectorAll('tbody tr');
+  
+    // Para cada producto, actualiza la cantidad total y el precio total
+    productos.forEach(function(producto) {
+      var id = producto.id.replace('fila-', '');
+  
+      // Obtén la cantidad y el precio del producto
+      var cantidad = parseInt(document.querySelector('#cantidad-tabla-' + id).textContent, 10);
+      var precio = parseFloat(document.querySelector('#subtotal-' + id).textContent.replace('$', ''));
+  
+      // Actualiza la cantidad total y el precio total
+      totalCantidad += cantidad;
+      totalPrecio += precio;
+    });
+  
+    // Actualiza la cantidad total y el precio total en la página
+    document.querySelector('#totalCantidad').textContent = totalCantidad;
+    document.querySelector('#totalPrecio').textContent = '$' + totalPrecio.toFixed(2);
+  }
+  
+ 
+
+  function actualizarCantidad(id) {
+    var inputCantidad = document.querySelector('#cantidad-' + id);
+    var cantidad = parseInt(inputCantidad.value, 10);
+    var precioElemento = document.querySelector('#precio-' + id);
+    var precio = parseFloat(precioElemento.textContent.replace('$', ''));
+    var subtotalElemento = document.querySelector('#subtotal-' + id);
+    var subtotal = precio * cantidad;
+    subtotalElemento.textContent = '$' + subtotal.toFixed(2);
+
+    var totalCantidadElemento = document.querySelector('#totalCantidad');
+    var totalPrecioElemento = document.querySelector('#totalPrecio'); // Cambiado de '#total' a '#totalPrecio'
+    var filasProducto = document.querySelectorAll('tbody tr');
+    var totalCantidad = 0;
+    var totalPrecio = 0;
+    filasProducto.forEach(function(fila) {
+        var id = fila.id.split('-')[1]; // Extraer el número del id de la fila
+        var cantidadElemento = document.querySelector('#cantidad-tabla-' + id);
+        var subtotalElemento = document.querySelector('#subtotal-' + id);
+        var cantidad = parseInt(cantidadElemento.textContent, 10);
+        var subtotal = parseFloat(subtotalElemento.textContent.replace('$', ''));
+        totalCantidad += cantidad;
+        totalPrecio += subtotal;
+    });
+    totalCantidadElemento.textContent = totalCantidad;
+    totalPrecioElemento.textContent = '$' + totalPrecio.toFixed(2);
+}
 document.querySelector('.boton-vaciar').addEventListener('click', function(e) {
   // Eliminar los productos de la vista
   var filasProducto = document.querySelectorAll('tbody tr');
@@ -10,8 +92,6 @@ document.querySelector('.boton-vaciar').addEventListener('click', function(e) {
 
 document.querySelector('.boton-continuar-compra').addEventListener('click', function(e){
   e.preventDefault();
-
-  console.log('Botón Continuar Compra presionado');
 
   var filasProducto = document.querySelectorAll('tbody tr');
   var productos = [];
@@ -29,7 +109,6 @@ document.querySelector('.boton-continuar-compra').addEventListener('click', func
     }
   });
 
-  console.log('Productos recogidos:', productos);
 
   var totalCantidad = productos.reduce(function(total, producto) {
     return total + producto.cantidad;
@@ -47,8 +126,6 @@ document.querySelector('.boton-continuar-compra').addEventListener('click', func
   mensaje += 'Total de productos: ' + totalCantidad + '\n';
   mensaje += 'Total: $' + totalPrecio;
 
-  console.log('Mensaje generado:', mensaje);
-
   // Eliminar espacios en blanco y saltos de línea innecesarios
   mensaje = mensaje.replace(/\s+/g, ' ').trim();
 
@@ -57,7 +134,8 @@ document.querySelector('.boton-continuar-compra').addEventListener('click', func
   console.log('Redirigiendo a WhatsApp:', whatsapp_url);
   window.location.href = whatsapp_url;
 
-  // Enviar el formulario programáticamente
-  console.log('Enviando formulario');
   document.querySelector('#form-compra').submit();
 });
+
+
+
