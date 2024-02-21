6fb3e889ef4f60bbd4b4ea8036f8f9252e0eb703
Walter Cordoba <waltercordobadev@gmail.com>
2024-02-21 19:07:13 -0300

    cambio errores app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -42,8 +41,7 @@ app.use((req, res, next) => {
     res.redirect('/');
     return;
   }
-
-  // Si la sesión está activa o el usuario no ha iniciado sesión, continúa con la siguiente función middleware
+ // Si la sesión está activa o el usuario no ha iniciado sesión, continúa con la siguiente función middleware
   next();
 });
 
@@ -70,20 +68,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/productos', productosRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 module.exports = app;
