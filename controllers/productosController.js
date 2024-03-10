3eeb4bf7aa730899307a86ae598ec50fb497d935
Walter Cordoba <waltercordobadev@gmail.com>
2024-03-10 14:35:17 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -553,7 +553,7 @@ actualizarPorProveedor : function(req, res) {
     }
 
     // Llamar al método del modelo para actualizar los precios
-    Producto.actualizarPreciosPorProveedor(proveedorId, porcentajeCambio, function(err) {
+    producto.actualizarPreciosPorProveedor(proveedorId, porcentajeCambio, function(err) {
         if (err) {
             // Manejar el error como prefieras
             console.error(err);
