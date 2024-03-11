0164edd51d0efc9956bea1a27ce85ae85ce7427d
Walter Cordoba <waltercordobadev@gmail.com>
2024-03-10 21:49:21 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -55,16 +55,14 @@ module.exports = {
     
                                 let modelosPorMarca = []; // Inicializar modelosPorMarca con un array vacío
 
-                                Promise.all([categoriasPromise, marcasPromise, modelosPromise])
-                                .then(([categorias, marcas, modelos]) => {
-                                    // Renderizar la vista con los productos, categorías, marcas y modelos
-                                    res.render('productos', { productos, categorias, marcas, modelosPorMarca: modelos });
-                                })
-                                .catch(error => {
-                                    console.log('Error al obtener categorías, marcas o modelos:', error);
-                                    // Renderizar la vista con los productos, categorías, marcas y un array vacío para modelosPorMarca
-                                    res.render('productos', { productos, categorias, marcas, modelosPorMarca: [] });
-                                });
+Promise.all([categoriasPromise, marcasPromise, modelosPromise])
+.then(([categorias, marcas, modelos]) => {
+    res.render('productos', { productos, categorias, marcas, modelosPorMarca: modelos });
+})
+.catch(error => {
+    console.log('Error al obtener categorías, marcas o modelos:', error);
+    res.render('productos', { productos, categorias, marcas, modelosPorMarca: [] });
+});
                             }
                         }
                     });
