35a604656c52fedb3e6e21fef8544cd3587011cd
Walter Cordoba <waltercordobadev@gmail.com>
2024-03-16 14:50:55 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -14,37 +14,40 @@ $(document).ready(function(){
   });
 
   $('#boton-buscar').click(function(){
-  var categoriaId = $('#categoria_id').val();
-  var marcaId = $('#id_marca').val();
-  var modeloId = $('#modelo_id').val();
-  $.ajax({
-    url: '/productos/buscar',
-    type: 'GET',
-    data: {
-      categoria_id: categoriaId,
-      marca_id: marcaId,
-      modelo_id: modeloId
-    },
-    success: function(data) {
-      $('#contenedor-productos').empty();
-      data.forEach(function(producto) {
-        var productoHtml = '<div class="card">' +
-          '<div class="cover__card">' +
-          '<img src="../../images/' + producto.imagen + '" alt="Imagen de ' + producto.nombre + '">' +
-          '</div>' +
-          '<div class="titulo-producto">' +
-          '<h3 class="nombre">' + producto.nombre + '</h3>' +
-          '</div>' +
-          '<div class="precio-producto">' +
-          '<p class="precio">$' + producto.precio + '</p>' +
-          '</div>' +
-          '<div class="cantidad-producto">' +
-          '<a href="/productos/carrito/agregar/' + producto.id + '" class="agregar-carrito">Agregar al carrito</a>' +
-          '</div>' +
-          '</div>';
-        $('#contenedor-productos').append(productoHtml);
-      });
-    }
+    var categoriaId = $('#categoria_id').val();
+    var marcaId = $('#id_marca').val();
+    var modeloId = $('#modelo_id').val();
+    $.ajax({
+      url: '/productos/buscar',
+      type: 'GET',
+      data: {
+        categoria_id: categoriaId,
+        marca_id: marcaId,
+        modelo_id: modeloId
+      },
+      success: function(data) {
+        $('#contenedor-productos').empty();
+        data.forEach(function(producto) {
+          var productoHtml = '<div class="card">' +
+            '<div class="cover__card">' +
+            '<img src="../../images/' + producto.imagen + '" alt="Imagen de ' + producto.nombre + '">' +
+            '</div>' +
+            '<div class="titulo-producto">' +
+            '<h3 class="nombre">' + producto.nombre + '</h3>' +
+            '</div>' +
+            '<div class="categoria-producto">' +
+            '<h6 class="categoria">' + producto.categoria + '</h6>' +
+            '</div>' +
+            '<div class="precio-producto">' +
+            '<p class="precio">$' + producto.precio + '</p>' +
+            '</div>' +
+            '<div class="cantidad-producto">' +
+            '<a href="/productos/carrito/agregar/' + producto.id + '" class="agregar-carrito">Agregar al carrito</a>' +
+            '</div>' +
+            '</div>';
+          $('#contenedor-productos').append(productoHtml);
+        });
+      }
+    });
   });
-});
 });
