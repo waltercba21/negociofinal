f8fdde0bdefd2ed79871073358f6f6c080d7f3d5
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-03 21:16:45 -0300

    cambios en el javascript

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -18,9 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
 function buscarProductos() {
   const consulta = entrada.value;
 
-  let url = 'http://www.autofaros.com.ar/productos/api/buscar?';
+  let url = 'http://www.autofaros.com.ar/productos/api/';
   if (consulta) {
-    url += `query=${consulta}&`;
+    url += `buscar?query=${consulta}`;
   }
   fetch(url, {mode:'cors', credentials:'include'})
   .then(response => response.json())
