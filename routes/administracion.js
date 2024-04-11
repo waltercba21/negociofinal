cb63182083d096aa64fdb4e24ee17b5cd3aa462e
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-11 17:47:48 -0300

    cambios en el archivo de rutas

--- routes/administracion.js
+++ routes/administracion.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var administracionController = require('../controllers/administracionController');
 
 //Administración
-router.get('/administracion', administracionController.administracion);
+router.get('/', administracionController.administracion);
 router.get('/facturas', administracionController.facturas);
 router.get('/presupuestos', administracionController.presupuestos);
 
