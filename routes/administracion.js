2844e1947ae9b2847f71314c83b5520b621bdf29
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-11 11:23:18 -0300

    cambios

--- routes/administracion.js
+++ routes/administracion.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var multer = require('multer');
-const administracionController = require('../controllers/administracionController');  
-var fecha = Date.now();
-var methodOverride = require('method-override')
-
+var administracionController = require('../controllers/administracionController');
 
 //Administración
-// En tu archivo de rutas
 router.get('/administracion', administracionController.administracion);
 router.get('/facturas', administracionController.facturas);
 router.get('/presupuestos', administracionController.presupuestos);
 
+module.exports = router;
