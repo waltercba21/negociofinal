591c8b520421181e4c78b36a3c604b9bbfbc34ea
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-12 12:28:31 -0300

    cambios

--- public/js/listadoFacturas.js
+++ public/js/listadoFacturas.js
@@ -6,9 +6,6 @@ document.getElementById('filterForm').addEventListener('submit', function(event)
     var fechaPago = document.getElementById('fechaPago').value;
     var condicion = document.getElementById('condicion').value;
 
-    var fechaFacturaFormateada = fechaFactura ? fechaFactura.split('/').reverse().join('-') : null;
-    var fechaPagoFormateada = fechaPago ? fechaPago.split('/').reverse().join('-') : null;
-    
     fetch('/administracion/api/facturas', {
         method: 'POST',
         headers: {
@@ -16,47 +13,45 @@ document.getElementById('filterForm').addEventListener('submit', function(event)
         },
         body: JSON.stringify({
             proveedor: proveedor,
-            fechaFactura: fechaFacturaFormateada,
-            fechaPago: fechaPagoFormateada,
+            fechaFactura: fechaFactura,
+            fechaPago: fechaPago,
             condicion: condicion
         })
     })
     .then(response => response.json())
     .then(data => {
-           // Selecciona el cuerpo de la tabla
-    var tbody = document.querySelector('tbody');
+        var tbody = document.querySelector('tbody');
 
-    // Limpia el cuerpo de la tabla
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
 
-    // Agrega las nuevas filas a la tabla
-    data.forEach(function(factura) {
-        var tr = document.createElement('tr');
-    
-        var fechaFactura = new Date(factura.fecha);
-        var fechaFacturaFormateada = fechaFactura ? `${fechaFactura.getDate().toString().padStart(2, '0')}/${(fechaFactura.getMonth()+1).toString().padStart(2, '0')}/${fechaFactura.getFullYear()}` : '';
-    
-        var fechaPago = new Date(factura.fecha_pago);
-        var fechaPagoFormateada = fechaPago ? `${fechaPago.getDate().toString().padStart(2, '0')}/${(fechaPago.getMonth()+1).toString().padStart(2, '0')}/${fechaPago.getFullYear()}` : '';
-    
-        tr.innerHTML = `
-            <td>${factura.id}</td>
-            <td>${factura.nombre_proveedor}</td>
-            <td>${fechaFacturaFormateada}</td>
-            <td>${factura.numero_factura}</td>
-            <td>${fechaPagoFormateada}</td>
-            <td>${factura.importe}</td>
-            <td>${factura.condicion}</td>
-            <td><img id="myImg" src="/uploads/comprobantes/${factura.comprobante_pago}" alt="Comprobante de pago" onclick="openModal(this)"></td>
-        `;
-    
-        tbody.appendChild(tr);
-    }); 
+        data.forEach(function(factura) {
+            var tr = document.createElement('tr');
+        
+            var fechaFactura = new Date(factura.fecha + 'T00:00:00');
+            var fechaFacturaFormateada = fechaFactura ? `${fechaFactura.getDate().toString().padStart(2, '0')}/${(fechaFactura.getMonth()+1).toString().padStart(2, '0')}/${fechaFactura.getFullYear()}` : '';
+        
+            var fechaPago = new Date(factura.fecha_pago + 'T00:00:00');
+            var fechaPagoFormateada = fechaPago ? `${fechaPago.getDate().toString().padStart(2, '0')}/${(fechaPago.getMonth()+1).toString().padStart(2, '0')}/${fechaPago.getFullYear()}` : '';
+        
+            tr.innerHTML = `
+                <td>${factura.id}</td>
+                <td>${factura.nombre_proveedor}</td>
+                <td>${fechaFacturaFormateada}</td>
+                <td>${factura.numero_factura}</td>
+                <td>${fechaPagoFormateada}</td>
+                <td>${factura.importe}</td>
+                <td>${factura.condicion}</td>
+                <td><img id="myImg" src="/uploads/comprobantes/${factura.comprobante_pago}" alt="Comprobante de pago" onclick="openModal(this)"></td>
+            `;
+        
+            tbody.appendChild(tr);
+        }); 
     })
     .catch(error => console.error('Error:', error));
 });
+
 function openModal(img) {
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("img01");
