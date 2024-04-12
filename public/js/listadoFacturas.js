a14d7ab8d3083b90518ec1053d6277d7ce6cb5de
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-12 10:37:04 -0300

    cambios

--- public/js/listadoFacturas.js
+++ public/js/listadoFacturas.js
@@ -24,3 +24,14 @@ document.getElementById('filterForm').addEventListener('submit', function(event)
     })
     .catch(error => console.error('Error:', error));
 });
+function openModal(img) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+}
