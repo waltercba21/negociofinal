565009ef206603bc7f25227b2b99a60faa0eab16
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-16 21:11:40 -0300

    cambios

--- public/js/facturas.js
+++ public/js/facturas.js
@@ -1,8 +1,11 @@
 document.getElementById('mostrarFormulario').addEventListener('click', function() {
     var formulario = document.getElementById('formularioFacturas');
+    var fondoOscuro = document.getElementById('fondoOscuro');
     if (formulario.style.display === 'none') {
       formulario.style.display = 'block';
+      fondoOscuro.style.display = 'block';
     } else {
       formulario.style.display = 'none';
+      fondoOscuro.style.display = 'none';
     }
   });
