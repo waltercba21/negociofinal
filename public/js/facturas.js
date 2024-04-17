d836d567bc428aa31ccf63994f2369b9099d1d59
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-17 09:14:00 -0300

    cambios

--- public/js/facturas.js
+++ public/js/facturas.js
@@ -1,11 +1,24 @@
 document.getElementById('mostrarFormulario').addEventListener('click', function() {
-    var formulario = document.getElementById('formularioFacturas');
-    var fondoOscuro = document.getElementById('fondoOscuro');
-    if (formulario.style.display === 'none') {
+  var formulario = document.getElementById('formularioFacturas');
+  var fondoOscuro = document.getElementById('fondoOscuro');
+  if (formulario.style.display === 'none') {
       formulario.style.display = 'block';
       fondoOscuro.style.display = 'block';
-    } else {
+  } else {
       formulario.style.display = 'none';
       fondoOscuro.style.display = 'none';
-    }
-  });
+  }
+});
+
+// Evento de escucha para el botón "Cancelar"
+document.querySelector('.btn-facturas-guardar button[type="reset"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.getElementById('formularioFacturas').style.display = 'none';
+  document.getElementById('fondoOscuro').style.display = 'none';
+});
+
+// Evento de escucha para el fondo oscuro
+document.getElementById('fondoOscuro').addEventListener('click', function() {
+  document.getElementById('formularioFacturas').style.display = 'none';
+  this.style.display = 'none';
+});
