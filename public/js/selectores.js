7e146fcd1eb6293514117b150e3397b46596e79c
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-19 12:36:17 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -39,7 +39,28 @@ function buscarProductos() {
             contenedorProductos.innerHTML = '';
             productos.forEach(function(producto) {
                 var tarjetaProducto = document.createElement('div');
-                tarjetaProducto.textContent = producto.nombre; // Asegúrate de cambiar esto para que coincida con las propiedades de tus productos
+                tarjetaProducto.className = 'card';
+                tarjetaProducto.innerHTML = `
+                    <div class="cover__card">
+                        <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
+                    </div>
+                    <div class="titulo-producto">
+                        <h3 class="nombre">${producto.nombre}</h3>
+                    </div>
+                    <hr>
+                    <div class="categoria-producto">
+                        <h6 class="categoria">${producto.categoria}</h6>
+                    </div>
+                    <div class="descripcion" style="display: none;">
+                        ${producto.descripcion}
+                    </div>
+                    <div class="precio-producto">
+                        <p class="precio">$${producto.precio}</p>
+                    </div>
+                    <div class="cantidad-producto">
+                        <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
+                    </div>
+                `;
                 contenedorProductos.appendChild(tarjetaProducto);
             });
         })
