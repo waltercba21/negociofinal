83d1b25be66c49c8e0dd700a62a26f44af675216
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-19 11:38:41 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -1,26 +1,5 @@
-document.getElementById('id_marca').addEventListener('change', function() {
-    var marcaId = this.value;
-    console.log(marcaId); // Log del ID de la marca
-    fetch('/productos/modelos/' + marcaId)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(modelos) {
-            console.log(modelos); // Log de los modelos obtenidos
-            var modeloSelect = document.getElementById('modelo_id');
-            modeloSelect.innerHTML = '';
-            modelos.forEach(function(modelo) {
-                var option = document.createElement('option');
-                option.value = modelo.id;
-                option.textContent = modelo.nombre;
-                modeloSelect.appendChild(option);
-            });
-        })
-        .catch(function(error) {
-            console.error('Error:', error);
-        });
-});
-document.getElementById('boton-buscar').addEventListener('click', function() {
+// Función para realizar la búsqueda y actualizar la vista de productos
+function buscarProductos() {
     var categoriaId = document.getElementById('categoria_id').value;
     var marcaId = document.getElementById('id_marca').value;
     var modeloId = document.getElementById('modelo_id').value;
@@ -67,4 +46,9 @@ document.getElementById('boton-buscar').addEventListener('click', function() {
         .catch(function(error) {
             console.error('Error:', error);
         });
-});
+}
+
+// Evento de cambio para cada selector
+document.getElementById('categoria_id').addEventListener('change', buscarProductos);
+document.getElementById('id_marca').addEventListener('change', buscarProductos);
+document.getElementById('modelo_id').addEventListener('change', buscarProductos);
