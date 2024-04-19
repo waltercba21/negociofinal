1097004a95443b0b6be5ad3fb3c21b19feecb61e
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-19 12:12:32 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -16,31 +16,30 @@ function buscarModelos() {
                 option.textContent = modelo.nombre;
                 modeloSelect.appendChild(option);
             });
+            // Llama a buscarProductos después de que el selector de modelos se haya actualizado
+            buscarProductos();
         })
         .catch(function(error) {
             console.error('Error:', error);
         });
 }
+
 // Función para realizar la búsqueda y actualizar la vista de productos
-function buscarModelos() {
-    var marcaId = this.value;
-    console.log(marcaId); // Log del ID de la marca
-    fetch('/productos/modelos/' + marcaId)
+function buscarProductos() {
+    var categoriaId = document.getElementById('categoria_id').value;
+    var marcaId = document.getElementById('id_marca').value;
+    var modeloId = document.getElementById('modelo_id').value;
+
+    fetch('/productos/buscar?categoria=' + categoriaId + '&marca=' + marcaId + '&modelo=' + modeloId)
         .then(function(response) {
             return response.json();
         })
-        .then(function(modelos) {
-            console.log(modelos); // Log de los modelos obtenidos
-            var modeloSelect = document.getElementById('modelo_id');
-            modeloSelect.innerHTML = '';
-            modelos.forEach(function(modelo) {
-                var option = document.createElement('option');
-                option.value = modelo.id;
-                option.textContent = modelo.nombre;
-                modeloSelect.appendChild(option);
+        .then(function(productos) {
+            var contenedorProductos = document.getElementById('contenedor-productos');
+            contenedorProductos.innerHTML = '';
+            productos.forEach(function(producto) {
+                // Aquí deberías crear y agregar las tarjetas de productos al contenedor
             });
-            // Llama a buscarProductos después de que el selector de modelos se haya actualizado
-            buscarProductos();
         })
         .catch(function(error) {
             console.error('Error:', error);
@@ -50,7 +49,6 @@ function buscarModelos() {
 // Evento de cambio para el selector de marca
 document.getElementById('id_marca').addEventListener('change', function() {
     buscarModelos.call(this);
-    buscarProductos();
 });
 
 // Evento de cambio para los otros selectores
