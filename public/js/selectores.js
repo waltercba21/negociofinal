2bc04a94e2965860a8b6c7a696f7e5c3f0dbb48c
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-19 08:44:49 -0300

    cambios

---
+++ public/js/selectores.js
@@ -0,0 +1,20 @@
+document.getElementById('id_marca').addEventListener('change', function() {
+    var marcaId = this.value;
+    fetch('/modelos/' + marcaId)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(modelos) {
+            var modeloSelect = document.getElementById('modelo_id');
+            modeloSelect.innerHTML = '';
+            modelos.forEach(function(modelo) {
+                var option = document.createElement('option');
+                option.value = modelo.id;
+                option.textContent = modelo.nombre;
+                modeloSelect.appendChild(option);
+            });
+        })
+        .catch(function(error) {
+            console.error('Error:', error);
+        });
+});
