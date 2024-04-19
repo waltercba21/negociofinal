aef3862ad8581d1f6251881b5780e4dfbfd7fcff
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-19 20:59:40 -0300

    cambios

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -102,28 +102,32 @@ function cargarProductos() {
 
 function mostrarProductos(productos) {
   contenedorProductosBuscador.innerHTML = '';
-  productos.forEach(producto => {
-    const tarjetaProducto = `
-    <div class="card"> 
-    <div class="cover__card">
-      <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
+  if (productos.length === 0) {
+    contenedorProductosBuscador.innerHTML = '<p>No se encontraron productos que coincidan con los criterios seleccionados.</p>';
+  } else {
+    productos.forEach(producto => {
+      const tarjetaProducto = `
+      <div class="card"> 
+      <div class="cover__card">
+        <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
+      </div>
+      <div class="titulo-producto">
+        <h3 class="nombre">${producto.nombre}</h3>
+      </div>
+      <hr>
+      <div class="categoria-producto">
+        <h6 class="categoria">${producto.categoria_nombre}</h6>
+      </div>
+      <hr>
+      <div class="precio-producto">
+        <p class="precio">$${producto.precio}</p>
+      </div>
+      <div class="cantidad-producto">
+        <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
+      </div>
     </div>
-    <div class="titulo-producto">
-      <h3 class="nombre">${producto.nombre}</h3>
-    </div>
-    <hr>
-    <div class="categoria-producto">
-      <h6 class="categoria">${producto.categoria_nombre}</h6>
-    </div>
-    <hr>
-    <div class="precio-producto">
-      <p class="precio">$${producto.precio}</p>
-    </div>
-    <div class="cantidad-producto">
-      <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
-    </div>
-  </div>
-`;
-    contenedorProductosBuscador.innerHTML += tarjetaProducto;
-  });
+  `;
+      contenedorProductosBuscador.innerHTML += tarjetaProducto;
+    });
+  }
 }
