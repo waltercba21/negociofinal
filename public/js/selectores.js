3b311d29025455dbd81412e86336f2d4c0baf44a
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-23 18:15:08 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -57,7 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('data:', data);
         if (!data || !Array.isArray(data.productos)) {
           console.error('La respuesta del servidor no tiene la forma esperada');
-          return;
         }
 
         console.log('productos:', data.productos);
