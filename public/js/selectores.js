636eb70180c4f8af2c921b6c51df85e0e07bd6e6
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-23 18:44:22 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -1,32 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOMContentLoaded event fired');
   let marcaSelect = document.querySelector('#id_marca');
   let modeloSelect = document.querySelector('#modelo_id');
   let contenedorProductos = document.querySelector('#contenedor-productos');
 
-  console.log('marcaSelect:', marcaSelect);
-  console.log('modeloSelect:', modeloSelect);
-  console.log('contenedorProductos:', contenedorProductos);
-
   if (!marcaSelect || !modeloSelect || !contenedorProductos) {
-    console.error('No se encontraron los elementos necesarios en el DOM');
     return;
   }
 
   marcaSelect.addEventListener('change', function() {
-    console.log('marcaSelect change event fired');
     modeloSelect.innerHTML = '';
 
     fetch(`/productos/modelos/${marcaSelect.value}`)
       .then(response => {
-        console.log('fetch response:', response);
         if (!response.ok) {
           throw new Error('Error HTTP: ' + response.status);
         }
         return response.json();
       })
       .then(modelos => {
-        console.log('modelos:', modelos);
         modelos.forEach(modelo => {
           let option = document.createElement('option');
           option.value = modelo.id;
@@ -42,28 +33,21 @@ document.addEventListener('DOMContentLoaded', function() {
   modeloSelect.addEventListener('change', buscarProductos);
 
   function buscarProductos() {
-    console.log('buscarProductos function called');
     const url = `/productos/api/buscar?marca=${marcaSelect.value}&modelo=${modeloSelect.value}`;
-    console.log('fetch url:', url);
     fetch(url)
       .then(response => {
-        console.log('fetch response:', response);
         if (!response.ok) {
           throw new Error('Error HTTP: ' + response.status);
         }
         return response.json();
       })
-      .then(data => {
-        console.log('data:', data);
+      .then(data => { 
         if (!data || !Array.isArray(data.productos)) {
           console.error('La respuesta del servidor no tiene la forma esperada');
         }
-
-        console.log('productos:', data.productos);
         contenedorProductos.innerHTML = '';
 
         data.productos.forEach(producto => {
-          console.log('producto:', producto);
           let div = document.createElement('div');
           div.className = 'card';
           div.innerHTML = `
