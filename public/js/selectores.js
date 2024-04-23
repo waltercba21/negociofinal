6d62b0304b7e048a920f1202e71bb97860f1a6ee
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-23 17:36:26 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -38,50 +38,56 @@ document.addEventListener('DOMContentLoaded', function() {
     modeloSelect.addEventListener('change', buscarProductos);
   
     function buscarProductos() {
-        fetch(`/productos/api/buscar?marca=${marcaSelect.value}&modelo=${modeloSelect.value}`)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Error HTTP: ' + response.status);
-            }
-            return response.json();
-          })
-          .then(data => {
-            // Imprime los productos para depuración
-            console.log(data.productos);
-    
-            // Limpia el contenedor de productos
-            contenedorProductos.innerHTML = '';
-    
-            // Añade los productos al contenedor
-            data.productos.forEach(producto => {
-              let div = document.createElement('div');
-              div.className = 'card';
-              div.innerHTML = `
-                <div class="cover__card">
-                  <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
-                </div>
-                <div class="titulo-producto">
-                  <h3 class="nombre">${producto.nombre}</h3>
-                </div>
-                <hr>
-                <div class="categoria-producto">
-                  <h6 class="categoria">${producto.categoria}</h6>
-                </div>
-                <div class="descripcion" style="display: none;">
-                  ${producto.descripcion}
-                </div>
-                <div class="precio-producto">
-                  <p class="precio">$${producto.precio}</p>
-                </div>
-                <div class="cantidad-producto">
-                  <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
-                </div>
-              `;
-              contenedorProductos.appendChild(div);
-            });
-          })
-          .catch(error => {
-            console.error('Hubo un problema con la solicitud: ' + error);
+      fetch(`/productos/api/buscar?marca=${marcaSelect.value}&modelo=${modeloSelect.value}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Error HTTP: ' + response.status);
+          }
+          return response.json();
+        })
+        .then(data => {
+          // Verifica si la respuesta tiene la forma esperada
+          if (!data || !Array.isArray(data.productos)) {
+            console.error('La respuesta del servidor no tiene la forma esperada');
+            return;
+          }
+  
+          // Imprime los productos para depuración
+          console.log(data.productos);
+  
+          // Limpia el contenedor de productos
+          contenedorProductos.innerHTML = '';
+  
+          // Añade los productos al contenedor
+          data.productos.forEach(producto => {
+            let div = document.createElement('div');
+            div.className = 'card';
+            div.innerHTML = `
+              <div class="cover__card">
+                <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
+              </div>
+              <div class="titulo-producto">
+                <h3 class="nombre">${producto.nombre}</h3>
+              </div>
+              <hr>
+              <div class="categoria-producto">
+                <h6 class="categoria">${producto.categoria}</h6>
+              </div>
+              <div class="descripcion" style="display: none;">
+                ${producto.descripcion}
+              </div>
+              <div class="precio-producto">
+                <p class="precio">$${producto.precio}</p>
+              </div>
+              <div class="cantidad-producto">
+                <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
+              </div>
+            `;
+            contenedorProductos.appendChild(div);
           });
-      }
+        })
+        .catch(error => {
+          console.error('Hubo un problema con la solicitud: ' + error);
+        });
+    }
   });
