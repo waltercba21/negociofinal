6e4aa75c0a641422c6f647005d3dadc9fc91e01b
Walter Cordoba <waltercordobadev@gmail.com>
2024-04-24 08:52:30 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -1,60 +1,81 @@
-// selectores.js
 $(document).ready(function() {
-  // Obtén los elementos del DOM
-  const categoriaSelector = $('#categoria_id');
-  const marcaSelector = $('#id_marca');
-  const modeloSelector = $('#modelo_id');
-  const contenedorProductos = $('#contenedor-productos');
-
-  // Función para realizar la solicitud AJAX
-  function obtenerProductosFiltrados() {
-      const categoria = categoriaSelector.val();
-      const marca = marcaSelector.val();
-      const modelo = modeloSelector.val();
-
-      $.get('/productos/api/buscar', { categoria, marca, modelo }, function(data) {
-          renderizarProductos(data.productos);
+    // Obtén los elementos del DOM
+    const categoriaSelector = $('#categoria_id');
+    const marcaSelector = $('#id_marca');
+    const modeloSelector = $('#modelo_id');
+    const contenedorProductos = $('#contenedor-productos');
+    const entrada = $('#entradaBusqueda');
+  
+    // Función para realizar la solicitud AJAX
+    function obtenerProductosFiltrados() {
+        const categoria = categoriaSelector.val();
+        const marca = marcaSelector.val();
+        const modelo = modeloSelector.val();
+        const consulta = entrada.val();
+  
+        $.get('/productos/api/buscar', { categoria, marca, modelo, query: consulta }, function(data) {
+            renderizarProductos(data.productos);
+        });
+    }
+  
+    // Función para renderizar los productos
+    function renderizarProductos(productos) {
+        // Limpia el contenedor de productos
+        contenedorProductos.empty();
+  
+        if (productos.length === 0) {
+            contenedorProductos.append('<p>No se encontraron productos que coincidan con los criterios seleccionados.</p>');
+            return;
+        }
+  
+        // Genera el HTML para cada producto
+        productos.forEach(producto => {
+            const productoHTML = `
+                <div class="card">
+                    <div class="cover__card">
+                        <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
+                    </div>
+                    <div class="titulo-producto">
+                        <h3 class="nombre">${producto.nombre}</h3>
+                    </div>
+                    <hr>
+                    <div class="categoria-producto">
+                        <h6 class="categoria">${producto.categoria}</h6>
+                    </div>
+                    <div class="descripcion" style="display: none;">
+                        ${producto.descripcion}
+                    </div>
+                    <div class="precio-producto">
+                        <p class="precio">$${producto.precio}</p>
+                    </div>
+                    <div class="cantidad-producto">
+                        <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
+                    </div>
+                </div>
+            `;
+  
+            // Agrega el producto al contenedor
+            contenedorProductos.append(productoHTML);
+        });
+    }
+  
+    // Manejadores de eventos para los selectores
+    categoriaSelector.change(obtenerProductosFiltrados);
+    marcaSelector.change(function() {
+      // Limpia el select de modelos
+      modeloSelector.empty();
+  
+      // Obtiene los modelos para la marca seleccionada
+      $.get(`/productos/modelos/${marcaSelector.val()}`, function(data) {
+          // Añade los modelos al select
+          data.modelos.forEach(modelo => {
+              modeloSelector.append(new Option(modelo.nombre, modelo.id));
+          });
+  
+          // Realiza la búsqueda
+          obtenerProductosFiltrados();
       });
-  }
-
-  // Función para renderizar los productos
-  function renderizarProductos(productos) {
-      // Limpia el contenedor de productos
-      contenedorProductos.empty();
-
-      // Genera el HTML para cada producto
-      productos.forEach(producto => {
-          const productoHTML = `
-              <div class="card">
-                  <div class="cover__card">
-                      <img src="../../uploads/productos/${producto.imagen}" alt="Imagen de ${producto.nombre}">
-                  </div>
-                  <div class="titulo-producto">
-                      <h3 class="nombre">${producto.nombre}</h3>
-                  </div>
-                  <hr>
-                  <div class="categoria-producto">
-                      <h6 class="categoria">${producto.categoria}</h6>
-                  </div>
-                  <div class="descripcion" style="display: none;">
-                      ${producto.descripcion}
-                  </div>
-                  <div class="precio-producto">
-                      <p class="precio">$${producto.precio}</p>
-                  </div>
-                  <div class="cantidad-producto">
-                      <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
-                  </div>
-              </div>
-          `;
-
-          // Agrega el producto al contenedor
-          contenedorProductos.append(productoHTML);
-      });
-  }
-
-  // Manejadores de eventos para los selectores
-  categoriaSelector.change(obtenerProductosFiltrados);
-  marcaSelector.change(obtenerProductosFiltrados);
-  modeloSelector.change(obtenerProductosFiltrados);
-});
+    });
+    modeloSelector.change(obtenerProductosFiltrados);
+    entrada.on('input', obtenerProductosFiltrados);
+  });
