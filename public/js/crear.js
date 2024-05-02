c07cfa5dfdd8f74f5d0de30efdb153a7fa7bac18
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-02 16:45:14 -0300

    cambios

--- public/js/crear.js
+++ public/js/crear.js
@@ -65,16 +65,20 @@ if (!document.getElementById("precio_" + proveedor.id)) {
     contenedorPrecios.appendChild(divPrecio);
 
                     // Escucha el evento de cambio en el campo de entrada de precio
-                    inputPrecio.addEventListener('change', function() {
-                        // Calcula el precio con descuento
-                        var precioLista = parseFloat(this.value);
-                        var descuentoProveedor = parseFloat(proveedor.descuento); // Aquí necesitas obtener el descuento del proveedor
-                        var precioConDescuento = precioLista * (1 - descuentoProveedor / 100);
-                        // Muestra el precio con descuento en el campo de entrada de costo
-                        document.getElementById('costo').value = precioConDescuento.toFixed(2);
-                        // Muestra el descuento aplicado en el campo de entrada de descuento
-                        document.getElementById('descuento').value = descuentoProveedor.toFixed(2);
-                    });
+inputPrecio.addEventListener('change', function() {
+    // Obtiene el proveedor seleccionado actualmente
+    var proveedorActual = Array.from(document.getElementById('proveedores').selectedOptions).find(function(option) {
+        return option.value === proveedor.id;
+    });
+    // Calcula el precio con descuento
+    var precioLista = parseFloat(this.value);
+    var descuentoProveedor = parseFloat(proveedorActual.getAttribute('data-descuento')); // Aquí necesitas obtener el descuento del proveedor
+    var precioConDescuento = precioLista * (1 - descuentoProveedor / 100);
+    // Muestra el precio con descuento en el campo de entrada de costo
+    document.getElementById('costo').value = precioConDescuento.toFixed(2);
+    // Muestra el descuento aplicado en el campo de entrada de descuento
+    document.getElementById('descuento').value = descuentoProveedor.toFixed(2);
+});
                 }
                 if (!document.getElementById("codigo_" + proveedor.id)) {
                     var divCodigo = document.createElement('div');
