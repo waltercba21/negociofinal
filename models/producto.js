d85de0cc70b25954ffd202d6976bf13adf634bb2
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-03 19:17:41 -0300

    cambios

--- models/producto.js
+++ models/producto.js
@@ -18,19 +18,16 @@ obtenerTotal: function (conexion, funcion) {
 obtenerPorId: function (conexion, id, funcion) {
     conexion.query('SELECT productos.*, categorias.nombre AS categoria_nombre FROM productos INNER JOIN categorias ON productos.categoria_id = categorias.id WHERE productos.id = ?', [id], funcion);
   },
-  insertarProductoYProveedor : function(conexion, imagen, nombre, descripcion, categoria, marca, modelo, costo, utilidad, precio, proveedor_id, codigo, precio_lista, funcion) {
+  insertarProductoYProveedor : function(conexion, imagen, nombre, descripcion, categoria, marca, modelo, costo, utilidad, precio, proveedor_id, codigo, precio_lista) {
     // Primero, inserta el nuevo producto en la tabla 'productos'
     conexion.query('INSERT INTO productos (imagen, nombre, descripcion, categoria_id, marca_id, modelo_id, costo, utilidad, precio, codigo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
     [imagen, nombre, descripcion, categoria, marca, modelo, costo, utilidad, precio, codigo], function(error, results) {
-        if (error) return funcion(error);
+        if (error) throw error;
 
         // Luego, usa el 'id' que se generó para insertar el nuevo producto en la tabla 'producto_proveedor'
         conexion.query('INSERT INTO producto_proveedor (producto_id, proveedor_id, precio_lista) VALUES (?, ?, ?)',
         [results.insertId, proveedor_id, precio_lista], function(error, results) {
-            if (error) return funcion(error);
-
-            // Llama a la función de callback con los resultados de la segunda inserción
-            funcion(null, results);
+            if (error) throw error;
         });
     });
 },
