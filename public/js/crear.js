60ada942e230a7b79db967637371d1763f11ee0a
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-03 10:03:41 -0300

    cambios

--- public/js/crear.js
+++ public/js/crear.js
@@ -13,33 +13,35 @@ $('#marca').change(function() {
 // AGREGAR PROVEEDOR AL PRODUCTO 
 $(document).ready(function() {
     var proveedorTemplate = `
-        <div class="form-group-crear">
-            <label for="proveedores">Proveedores:</label>
-            <select id="proveedores" name="proveedores" multiple>
-                <% proveedores.forEach(function(proveedor) { %>
-                    <option value="<%= proveedor.id %>" data-descuento="<%= proveedor.descuento %>"><%= proveedor.nombre %></option>
-                <% }); %>
-            </select>
-        </div>
-        <div id="codigo" class="form-group-crear"></div>
-        <div class="form-group-crear">
-            <label for="precio_lista">Precio de Lista:</label>
-            <input id="precio_lista" class="form-control" type="number" name="precio_lista">
-        </div>
-        <div class="form-group-crear">
-            <label for="descuento">Descuento:</label>
-            <input id="descuento" class="form-control" type="number" name="descuento">
-        </div>
-    `;
-
-    $('#addProveedor').click(function() {
-        $('#proveedoresContainer').append(proveedorTemplate);
-    });
+    <div class="form-group-crear">
+        <label for="proveedores">Proveedores:</label>
+        <select class="proveedores" name="proveedores[]" multiple>
+            <% proveedores.forEach(function(proveedor) { %>
+                <option value="<%= proveedor.id %>" data-descuento="<%= proveedor.descuento %>"><%= proveedor.nombre %></option>
+            <% }); %>
+        </select>
+    </div>
+    <div class="form-group-crear">
+        <label for="codigo">Código:</label>
+        <input class="codigo" type="text" name="codigo[]">
+    </div>
+    <div class="form-group-crear">
+        <label for="precio_lista">Precio de Lista:</label>
+        <input class="precio_lista" class="form-control" type="number" name="precio_lista[]">
+    </div>
+    <div class="form-group-crear">
+        <label for="descuento">Descuento:</label>
+        <input class="descuento" class="form-control" type="number" name="descuento[]">
+    </div>
+`;
+$('#addProveedor').click(function() {
+    $('#proveedoresContainer').append(proveedorTemplate);
+});
 
     // ACTUALIZAR DINÁMICAMENTE EL DESCUENTO
-    $(document).on('change', '#proveedores', function() {
+    $(document).on('change', '.proveedores', function() {
         var selectedOption = $(this).find('option:selected');
         var descuento = selectedOption.data('descuento');
-        $('#descuento').val(descuento);
+        $(this).closest('.form-group-crear').find('.descuento').val(descuento);
     });
 });
