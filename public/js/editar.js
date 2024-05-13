d97ea5d5f7387d53ed2d8387180ed66e83c494ec
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-13 11:43:12 -0300

    cambios

--- public/js/editar.js
+++ public/js/editar.js
@@ -31,48 +31,56 @@ $(document).ready(function() {
     });
 });
 
-function attachChangeEventHandlers() {
-    $('.precio_lista, .descuentos_proveedor_id').off('change').on('change', function() {
-        var precioLista = parseFloat($(this).val());
-        var descuento = parseFloat($(this).closest('.form-group-crear').nextAll().find('.descuentos_proveedor_id').val());
-        var costoNeto = precioLista - (precioLista * descuento / 100);
-        $(this).closest('.form-group-crear').nextAll().find('.costo_neto').val(costoNeto.toFixed(2)); 
+$(document).on('change', '.precio_lista, .descuentos_proveedor_id', function() {
+    var precioLista = parseFloat($(this).val());
+    var descuento = parseFloat($(this).closest('.form-group-crear').nextAll().find('.descuentos_proveedor_id').val());
+    var costoNeto = precioLista - (precioLista * descuento / 100);
+    $(this).closest('.form-group-crear').nextAll().find('.costo_neto').val(costoNeto.toFixed(2)); 
 
-        // Calcular el 'Costo Neto Con IVA'
-        var IVA = parseFloat($(this).closest('.form-group-crear').nextAll().find('.IVA').val());
-        var costoIVA = costoNeto + (costoNeto * IVA / 100);
-        $(this).closest('.form-group-crear').nextAll().find('.costo_iva').val(costoIVA.toFixed(2));
+    // Calcular el 'Costo Neto Con IVA'
+    var IVA = parseFloat($(this).closest('.form-group-crear').nextAll().find('.IVA').val());
+    var costoIVA = costoNeto + (costoNeto * IVA / 100);
+    $(this).closest('.form-group-crear').nextAll().find('.costo_iva').val(costoIVA.toFixed(2));
 
-        // Disparar el evento de cambio para #utilidad
-        $('#utilidad').trigger('change');
-    });
-}
+    // Disparar el evento de cambio para #utilidad
+    $('#utilidad').trigger('change');
+});
 
 //AGREGAR PROVEEDORES
 var proveedorCount = 0; // Añadir un contador para los proveedores
 var proveedorTemplate = function(id) {
     return `
-    <div class="form-group-crear">
-    <label for="proveedores${id}">Proveedores: <span class="nombre_proveedor"></span></label>
-    <select class="proveedores" id="proveedores${id}" name="proveedores[]" multiple>
-        <!-- Aquí se agregarán las opciones de proveedores con JavaScript -->
-    </select>
-    </div>
-    <div class="form-group-crear">
-        <label for="precio_lista${id}">Precio de Lista:</label>
-        <input class="precio_lista" id="precio_lista${id}" type="number" name="precio_lista">
-    </div>
-    <div class="form-group-crear">
-        <label for="codigo${id}">Código:</label>
-        <input class="codigo" id="codigo${id}" type="text" name="codigo">
-    </div>
-    <div class="form-group-crear">
-    <label for="descuentos_proveedor_id${id}">Descuento:</label>
-    <input class="descuentos_proveedor_id" id="descuentos_proveedor_id${id}" class="form-control" type="number" name="descuentos_proveedor_id[]" readonly>
-    </div>
-    <div class="form-group-crear">
-    <label for="costo${id}">Costo Proveedor:</label>
-    <input class="costo" id="costo${id}" class="form-control" type="number" name="costo[]" readonly>
+    <div class="proveedor">
+        <div class="form-group-crear">
+            <label for="proveedores${id}">Proveedores: <span class="nombre_proveedor"></span></label>
+            <select class="proveedores" id="proveedores${id}" name="proveedores[]" multiple>
+                <!-- Aquí se agregarán las opciones de proveedores con JavaScript -->
+            </select>
+        </div>
+        <div class="form-group-crear">
+            <label for="codigo${id}">Código:</label>
+            <input class="codigo" id="codigo${id}" type="text" name="codigo">
+        </div>
+        <div class="form-group-crear">
+            <label for="precio_lista${id}">Precio de Lista:</label>
+            <input class="precio_lista form-control" id="precio_lista${id}" type="number" name="precio_lista[]">
+        </div>
+        <div class="form-group-crear">
+            <label for="descuentos_proveedor_id${id}">Descuentos Proveedor:</label>
+            <input class="descuentos_proveedor_id form-control" id="descuentos_proveedor_id${id}" type="text" name="descuentos_proveedor_id[]" readonly>
+        </div>
+        <div class="form-group-crear">
+            <label for="costo_neto${id}">Costo Neto:</label>
+            <input class="costo_neto form-control" id="costo_neto${id}" type="number" name="costo_neto[]" readonly>
+        </div>
+        <div class="form-group-crear">
+            <label for="IVA${id}">IVA:</label>
+            <input class="IVA form-control" id="IVA${id}" type="number" name="IVA[]" value="21" readonly>
+        </div>
+        <div class="form-group-crear">
+            <label for="costo_iva${id}">Costo Neto Con IVA :</label>
+            <input class="costo_iva form-control" id="costo_iva${id}" type="number" name="costo_iva[]">
+        </div>
     </div>
     `;
 };
@@ -110,18 +118,17 @@ $('#addProveedor').click(function(event) {
     });
 });
 
-    // Adjuntar el controlador de eventos change al elemento .precio_lista
-    newProveedor.find('.precio_lista').change(function() {
-        var precioLista = parseFloat($(this).val());
-        var descuento = parseFloat($(this).closest('.form-group-crear').nextAll().find('.descuento').val());
-        var costo = precioLista - (precioLista * descuento / 100);
-        $(this).closest('.form-group-crear').nextAll().find('.costo').val(costo.toFixed(2));
-    });
+newProveedor.find('.precio_lista').change(function() {
+    var precioLista = parseFloat($(this).val());
+    // Buscar el valor del descuento en un elemento con la clase .descuentos_proveedor_id
+    var descuento = parseFloat($(this).closest('.form-group-crear').nextAll().find('.descuentos_proveedor_id').val());
+    var costo = precioLista - (precioLista * descuento / 100);
+    $(this).closest('.form-group-crear').nextAll().find('.costo').val(costo.toFixed(2));
+});
 
     newProveedor.find('.proveedores').first().trigger('change');
 
     proveedorCount++; // Incrementar el contador de proveedores
-    attachChangeEventHandlers();
 });
 
 $('#utilidad').change(function() {
