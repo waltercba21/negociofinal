3349377189367a60166d65569558e67109228390
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-13 12:32:32 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -293,6 +293,57 @@ module.exports = {
             res.status(500).send('Hubo un error al intentar eliminar el producto');
         }
     },
+    editar: function(req, res) {
+        let productoResult;
+        let responseSent = false;
+        producto.retornarDatosId(conexion, req.params.id).then(result => {
+            console.log('Resultado de retornarDatosId:', result);
+            if (!result) {
+                console.error("No se encontró el producto con el id:", req.params.id);
+                res.status(404).send("No se encontró el producto");
+                responseSent = true;
+                return;
+            }
+            productoResult = result;
+            // Obtener los datos de producto_proveedor
+            producto.retornarDatosProveedor(conexion, req.params.id).then(productoProveedorResult => {
+                // Obtener las categorías, marcas, proveedores, modelos y descuentos de proveedores
+                Promise.all([
+                    producto.obtenerCategorias(conexion),
+                    producto.obtenerMarcas(conexion),
+                    producto.obtenerProveedores(conexion),
+                    producto.obtenerModelosPorMarca(conexion, productoResult.marca), // Asegúrate de que estás obteniendo los modelos para la marca correcta
+                    producto.obtenerDescuentosProveedor(conexion)
+                ]).then(([categoriasResult, marcasResult, proveedoresResult, modelosResult, descuentosProveedoresResult]) => {
+                    // Renderizar la vista 'editar' con todos los datos
+                    res.render('editar', {
+                        producto: productoResult,
+                        productoProveedor: productoProveedorResult,
+                        categorias: categoriasResult,
+                        marcas: marcasResult,
+                        proveedores: proveedoresResult,
+                        modelos: modelosResult,
+                        descuentosProveedor: descuentosProveedoresResult
+                    });
+                }).catch(error => {
+                    console.error("Error al obtener los datos:", error);
+                    if (!responseSent) {
+                        res.status(500).send("Error al obtener los datos");
+                    }
+                });
+            }).catch(error => {
+                console.error("Error al obtener los datos de producto_proveedor:", error);
+                if (!responseSent) {
+                    res.status(500).send("Error al obtener los datos de producto_proveedor");
+                }
+            });
+        }).catch(error => {
+            console.error("Error al obtener los datos:", error);
+            if (!responseSent) {
+                res.status(500).send("Error al obtener los datos");
+            }
+        });
+    },
     actualizar: function (req, res) {
         console.log('Iniciando la actualización del producto');
     
