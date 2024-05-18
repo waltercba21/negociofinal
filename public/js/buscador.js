6fd0814a504391cada56b1956c81b938b6b48bb0
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-18 20:25:24 -0300

    cambios

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -132,6 +132,7 @@ function mostrarProductos(productos) {
       </div>
       <div class="cantidad-producto">
         <a href="/productos/carrito/agregar/${producto.id}" class="agregar-carrito">Agregar al carrito</a>
+        <a href="/productos/${producto.id}" class="ver-detalles">Ver detalles</a>
       </div>
     </div>
   `;
