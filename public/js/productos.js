2865a9a12adc91929266798554c98337ea3df581
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-19 20:13:07 -0300

    cambios

--- public/js/productos.js
+++ public/js/productos.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-    $('.carousel').each(function() {
-      var $carousel = $(this);
-      var $images = $carousel.find('.carousel__image');
-      var $prevButton = $carousel.find('.carousel__button--prev');
-      var $nextButton = $carousel.find('.carousel__button--next');
-      var index = 0;
-  
-      $prevButton.click(function() {
-        $images.eq(index).hide();
-        index--;
-        if (index < 0) {
-          index = $images.length - 1;
-        }
-        $images.eq(index).show();
-      });
-  
-      $nextButton.click(function() {
-        $images.eq(index).hide();
-        index++;
-        if (index >= $images.length) {
-          index = 0;
-        }
-        $images.eq(index).show();
-      });
+  $('.carousel').each(function() {
+    var $carousel = $(this);
+    var $images = $carousel.find('.carousel__image');
+    var $prevButton = $carousel.parent().find('.carousel__button:has(.fa-chevron-left)');
+    var $nextButton = $carousel.parent().find('.carousel__button:has(.fa-chevron-right)');
+    var index = 0;
+
+    $prevButton.click(function() {
+      $images.eq(index).hide();
+      index--;
+      if (index < 0) {
+        index = $images.length - 1;
+      }
+      $images.eq(index).show();
+    });
+
+    $nextButton.click(function() {
+      $images.eq(index).hide();
+      index++;
+      if (index >= $images.length) {
+        index = 0;
+      }
+      $images.eq(index).show();
     });
   });
+});
