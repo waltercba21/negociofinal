c5f34e040c66eaf49ccfd3f74f31e6109e535f0c
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-20 19:53:49 -0300

    cambios

--- public/js/editar.js
+++ public/js/editar.js
@@ -1,29 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const botonesEliminar = document.querySelectorAll('.eliminar-imagen');
-    botonesEliminar.forEach(function(boton) {
-        boton.addEventListener('click', function(event) {
-            event.preventDefault();
-            const imagenId = this.parentNode.getAttribute('data-imagen-id');
-            fetch('http://www.autofaros.com.ar/productos/eliminarImagen/' + imagenId, {
-    method: 'DELETE'
-})
-            .then(response => {
-                const contentType = response.headers.get('content-type');
-                if (!contentType || !contentType.includes('application/json')) {
-                    throw new TypeError("Oops, no obtuvimos JSON!");
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.success) {
-                    this.parentNode.remove();
-                } else {
-                    alert('Error al eliminar la imagen: ' + data.error);
-                }
-            })
-            .catch(error => {
-                alert('Error al eliminar la imagen: ' + error);
-            });
+$(document).ready(function() {
+    $('.eliminar-imagen').click(function() {
+        var imagenId = $(this).parent().data('imagen-id');
+        $.ajax({
+            url: '/productos/eliminarImagen/' + imagenId,
+            type: 'DELETE',
+            success: function(result) {
+                // Actualiza la interfaz de usuario para reflejar la eliminación de la imagen
+                $('div[data-imagen-id="' + imagenId + '"]').remove();
+            },
+            error: function(xhr, status, error) {
+                console.error('Error al eliminar la imagen:', error);
+            }
         });
     });
 });
