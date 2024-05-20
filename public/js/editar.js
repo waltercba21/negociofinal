506083cc15673ce3192e8be1573b2fcb0d481610
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-20 20:35:33 -0300

    cambios

--- public/js/editar.js
+++ public/js/editar.js
@@ -13,3 +13,22 @@ $('.eliminar-imagen').click(function(event) {
         }
     });
 });
+$('#imagen').on('change', function() {
+    // Obtén los archivos seleccionados
+    var archivos = this.files;
+    // Obtén el contenedor de las imágenes miniatura
+    var contenedor = $('.imagen-miniatura-contenedor');
+    // Por cada archivo seleccionado
+    for (var i = 0; i < archivos.length; i++) {
+        // Crea una URL de objeto para el archivo
+        var urlImagen = URL.createObjectURL(archivos[i]);
+        // Crea un nuevo div para la imagen miniatura
+        var div = $('<div>');
+        // Crea la imagen miniatura
+        var img = $('<img>').attr('src', urlImagen).addClass('imagen-miniatura');
+        // Añade la imagen al div
+        div.append(img);
+        // Añade el div al contenedor
+        contenedor.append(div);
+    }
+});
