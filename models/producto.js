7aee94ace5b5aedfd8f07ba064c0fff83117ec9d
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-20 20:09:55 -0300

    cambios

--- models/producto.js
+++ models/producto.js
@@ -641,14 +641,18 @@ insertarImagenProducto: function(conexion, datosImagen) {
         });
     });
 },
-eliminarImagen : function(id, callback) {
-    console.log('Eliminar imagen llamado con id:', id); 
-    const sql = 'DELETE FROM imagenes_producto WHERE id = ?';
-    conexion.query(sql, [id], function(err, results) {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, results);
+eliminarImagen : function(id) {
+    return new Promise((resolve, reject) => {
+        console.log('Eliminar imagen llamado con id:', id); // Imprime el id con el que se llamó la función
+        const sql = 'DELETE FROM imagenes_producto WHERE id = ?';
+        conexion.query(sql, [id], function(err, results) {
+            if (err) {
+                console.log('Error al ejecutar la consulta SQL:', err); // Imprime el error si hay uno
+                return reject(err);
+            }
+            console.log('Resultados de la consulta SQL:', results); // Imprime los resultados de la consulta SQL
+            resolve(results);
+        });
     });
 }
 }
