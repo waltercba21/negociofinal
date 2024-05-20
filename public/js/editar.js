bbda933f57025b5ba5504b748517bfc2131ac681
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-20 20:13:48 -0300

    cambios

--- public/js/editar.js
+++ public/js/editar.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
-    $('.eliminar-imagen').click(function() {
-        var imagenId = $(this).parent().data('imagen-id');
-        $.ajax({
-            url: '/productos/eliminarImagen/' + imagenId,
-            type: 'DELETE',
-            success: function(result) {
-                // Actualiza la interfaz de usuario para reflejar la eliminación de la imagen
-                $('div[data-imagen-id="' + imagenId + '"]').remove();
-            },
-            error: function(xhr, status, error) {
-                console.error('Error al eliminar la imagen:', error);
-            }
-        });
+$('.eliminar-imagen').click(function(event) {
+    event.preventDefault();
+    var imagenId = $(this).parent().data('imagen-id');
+    $.ajax({
+        url: '/productos/eliminarImagen/' + imagenId,
+        type: 'DELETE',
+        success: function(result) {
+            // Actualiza la interfaz de usuario para reflejar la eliminación de la imagen
+            $('div[data-imagen-id="' + imagenId + '"]').remove();
+        },
+        error: function(xhr, status, error) {
+            console.error('Error al eliminar la imagen:', error);
+        }
     });
 });
