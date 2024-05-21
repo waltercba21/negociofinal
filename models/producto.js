e8b6acb9b1cc425fcd4acd38fd93ecd662eee880
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-21 20:15:44 -0300

    cambios

--- models/producto.js
+++ models/producto.js
@@ -551,7 +551,7 @@ obtenerPorFiltros: function(conexion, categoria, marca, modelo) {
 },
 retornarDatosId: function(conexion, id) {
     return new Promise((resolve, reject) => {
-        conexion.query('SELECT productos.*, imagenes_producto.id AS imagen_id, imagenes_producto.imagen FROM productos LEFT JOIN imagenes_producto ON productos.id = imagenes_producto.producto_id WHERE productos.id = ?', [id], function(error, results, fields) {
+        conexion.query('SELECT productos.*, IFNULL(productos.costo_neto, 0) AS costo_neto, IFNULL(productos.costo_iva, 0) AS costo_iva, IFNULL(productos.utilidad, 0) AS utilidad, IFNULL(productos.precio_venta, 0) AS precio_venta, imagenes_producto.id AS imagen_id, imagenes_producto.imagen FROM productos LEFT JOIN imagenes_producto ON productos.id = imagenes_producto.producto_id WHERE productos.id = ?', [id], function(error, results, fields) {
             if (error) {
                 console.log("Error en la consulta:", error);
                 reject(error);
