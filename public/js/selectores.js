2a0eaf2583e0284afedac156734b1d8d2baa52bf
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-22 18:06:50 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -7,11 +7,14 @@ const modeloSelector = document.getElementById('modelo_id');
 marcaSelector.addEventListener('change', function() {
   // Obtén el id de la marca seleccionada
   const marcaId = this.value;
+  console.log(`Marca seleccionada: ${marcaId}`); // Agrega un console.log aquí
 
   // Haz una solicitud AJAX para obtener los modelos de esta marca
   fetch(`/modelos/${marcaId}`)
     .then(response => response.json())
     .then(data => {
+      console.log('Modelos recibidos:', data); // Agrega un console.log aquí
+
       // Limpia el selector de modelos
       modeloSelector.innerHTML = '<option value="" selected>Selecciona un modelo...</option>';
 
@@ -33,10 +36,14 @@ marcaSelector.addEventListener('change', function() {
     const marcaId = marcaSelector.value;
     const modeloId = modeloSelector.value;
 
+    console.log(`Categoría seleccionada: ${categoriaId}, Marca seleccionada: ${marcaId}, Modelo seleccionado: ${modeloId}`); // Agrega un console.log aquí
+
     // Haz una solicitud AJAX para obtener los productos que coinciden con los criterios seleccionados
-    fetch(`/productos/api/buscar?q=&categoria_id=${categoriaId}&marca_id=${marcaId}&modelo_id=${modeloId}`) // Cambiado id_marca a marca_id en la URL
+    fetch(`/productos/api/buscar?q=&categoria_id=${categoriaId}&marca_id=${marcaId}&modelo_id=${modeloId}`)
       .then(response => response.json())
       .then(data => {
+        console.log('Productos recibidos:', data); // Agrega un console.log aquí
+
         // Aquí puedes actualizar la interfaz de usuario con los productos obtenidos
         console.log(data);
       });
