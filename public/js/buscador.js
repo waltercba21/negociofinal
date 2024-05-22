9849a1487c0f35f28c0b97485d040cca157efad0
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-22 08:16:48 -0300

    cambios

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -183,3 +183,30 @@ document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function() {
   $('.carousel').carousel();
 });
+$(document).ready(function() {
+  $('.carousel').each(function() {
+    var $carousel = $(this);
+    var $images = $carousel.find('.carousel__image');
+    var $prevButton = $carousel.closest('.card').find('.carousel__button:has(.fa-chevron-left)');
+    var $nextButton = $carousel.closest('.card').find('.carousel__button:has(.fa-chevron-right)');
+    var index = 0;
+
+    $prevButton.click(function() {
+      $images.eq(index).hide();
+      index--;
+      if (index < 0) {
+        index = $images.length - 1;
+      }
+      $images.eq(index).show();
+    });
+
+    $nextButton.click(function() {
+      $images.eq(index).hide();
+      index++;
+      if (index >= $images.length) {
+        index = 0;
+      }
+      $images.eq(index).show();
+    });
+  });
+});
