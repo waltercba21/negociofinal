f37f4d69ebb72c215a35edbb0b9d14e12df9de2e
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-23 19:39:00 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -78,47 +78,47 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         const precio_venta = producto.precio_venta ? `$${Math.floor(producto.precio_venta).toLocaleString('de-DE')}` : 'Precio no disponible';
         const tarjetaProducto = `
-  <div class="card"> 
-    ${imagenes}
-    <div class="titulo-producto">
-      <h3 class="nombre">${producto.nombre}</h3>
-    </div>
-    <hr>
-    <div class="categoria-producto">
-      <h6 class="categoria">${producto.categoria.nombre}</h6>
-    </div>
-    <hr>
-    <div class="precio-producto">
-      <p class="precio">${precio_venta}</p>
-    </div>
-    <div class="cantidad-producto">
-      <a href="/productos/${producto.id}" class="card-link">Ver detalles</a>
-    </div>
-  </div>
-`;
+        <div class="card"> 
+          ${imagenes}
+          <div class="titulo-producto">
+            <h3 class="nombre">${producto.nombre}</h3>
+          </div>
+          <hr>
+          <div class="categoria-producto">
+            <h6 class="categoria">${producto.categoria}</h6>
+          </div>
+          <hr>
+          <div class="precio-producto">
+            <p class="precio">${precio_venta}</p>
+          </div>
+          <div class="cantidad-producto">
+            <a href="/productos/${producto.id}" class="card-link">Ver detalles</a>
+          </div>
+        </div>
+      `;
       contenedorProductos.innerHTML += tarjetaProducto;
       });
   }
-    // Ahora que las tarjetas de productos se han agregado al DOM, puedes agregar los controladores de eventos a los botones del carrusel
-    $(document).on('click', '.carousel__button', function() {
-      var $carousel = $(this).closest('.card').find('.carousel');
-      var $images = $carousel.find('.carousel__image');
-      var index = $images.index($carousel.find('.carousel__image:visible'));
   
-      if ($(this).find('.fa-chevron-left').length > 0) {
-        $images.eq(index).hide();
-        index--;
-        if (index < 0) {
-          index = $images.length - 1;
-        }
-      } else {
-        $images.eq(index).hide();
-        index++;
-        if (index >= $images.length) {
-          index = 0;
-        }
+  $(document).on('click', '.carousel__button', function() {
+    var $carousel = $(this).closest('.card').find('.carousel');
+    var $images = $carousel.find('.carousel__image');
+    var index = $images.index($carousel.find('.carousel__image:visible'));
+
+    if ($(this).find('.fa-chevron-left').length > 0) {
+      $images.eq(index).hide();
+      index--;
+      if (index < 0) {
+        index = $images.length - 1;
       }
-  
-      $images.eq(index).show();
-    });
+    } else {
+      $images.eq(index).hide();
+      index++;
+      if (index >= $images.length) {
+        index = 0;
+      }
+    }
+
+    $images.eq(index).show();
+  });
 });
