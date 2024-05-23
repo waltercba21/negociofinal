c4b946f7cbf3f569df3cbb1cc3bdb82101d71c4d
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-23 10:35:04 -0300

    cambios

--- routes/productos.js
+++ routes/productos.js
@@ -41,7 +41,7 @@ router.get('/modificarPorProveedor', productosController.modificarPorProveedor);
 router.post('/actualizarPorProveedor', productosController.actualizarPorProveedor);
 router.post('/actualizarPrecio', productosController.actualizarPrecio);
 router.get('/api', productosController.lista);
-router.post('/api/buscar', productosController.buscar);
+router.get('/api/buscar', productosController.buscar);
 router.get('/:id', productosController.detalle); 
 
 module.exports = router;
