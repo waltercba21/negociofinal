6cec92ca66747cb207d64e91a1b8aa4c1f314eb2
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-23 18:12:47 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -30,7 +30,6 @@ console.log(selectorCategoria.value, selectorMarca.value, selectorModelo.value);
 // Función para realizar la búsqueda
 const buscarProductos = async () => {
   let url = '/productos/api/filtrar';
-  console.log(url);
   const params = new URLSearchParams();
 
   if (selectorCategoria.value) {
@@ -50,63 +49,9 @@ const buscarProductos = async () => {
   }
 
   const respuesta = await fetch(url);
-  const productos = await respuesta.json();
+  const htmlProductos = await respuesta.text();
   const contenedorProductos = document.getElementById('contenedor-productos');
-  contenedorProductos.innerHTML = '';
-
-  if (productos.length === 0) {
-    return;
-  }
-  console.log(productos);
-  productos.forEach((producto, index) => {
-    console.log(producto);
-    let imagenes = '';
-    if (producto.imagenes && producto.imagenes.length > 0) {
-      producto.imagenes.forEach((imagen, i) => {
-        imagenes += `<img class="carousel__image ${i !== 0 ? 'hidden' : ''}" src="/uploads/productos/${imagen}" alt="Imagen de ${producto.nombre}">`;
-      });
-      imagenes = `
-        <div class="cover__card">
-          <div class="carousel">
-            ${imagenes}
-          </div>
-        </div>
-        <div class="carousel__buttons">
-          <button class="carousel__button">
-            <i class="fas fa-chevron-left"></i>
-          </button>
-          <button class="carousel__button">
-            <i class="fas fa-chevron-right"></i>
-          </button>
-        </div>
-      `;
-    } else {
-      imagenes = '<img src="/ruta/valida/a/imagen/por/defecto.jpg" alt="Imagen de ${producto.nombre}">';
-    }
-    const precio_venta = producto.precio_venta ? `$${Math.floor(producto.precio_venta).toLocaleString('de-DE')}` : 'Precio no disponible';
-    const tarjetaProducto = `
-    <div class="card"> 
-      ${imagenes}
-      <div class="titulo-producto">
-        <h3 class="nombre">${producto.nombre}</h3>
-      </div>
-      <hr>
-      <div class="categoria-producto">
-        <h6 class="categoria">${producto.categoria}</h6>
-      </div>
-      <hr>
-      <div class="precio-producto">
-        <p class="precio">${precio_venta}</p>
-      </div>
-      <div class="cantidad-producto">
-        <a href="/productos/${producto.id}" class="card-link">Ver detalles</a>
-      </div>
-    </div>
-  `;
-  console.log(tarjetaProducto);
-  contenedorProductos.innerHTML += tarjetaProducto;
-  console.log(contenedorProductos.innerHTML);
-  });
+  contenedorProductos.innerHTML = htmlProductos;
 };
 
 
