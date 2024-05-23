5b30f77e4a87e4edf1ae0bfacaa2618db542bf57
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-23 13:02:32 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -1,10 +1,17 @@
 document.getElementById('marca_id').addEventListener('change', function() {
   const marcaId = this.value;
-  fetch('/productos/modelos/' + marcaId) // Cambia '/modelosPorMarca/' a '/modelos/'
+  fetch('/productos/modelos/' + marcaId)
       .then(response => response.json())
       .then(modelos => {
           const modeloSelect = document.getElementById('modelo_id');
           modeloSelect.innerHTML = '';
+
+          // Agrega la opción "Selecciona un modelo"
+          const defaultOption = document.createElement('option');
+          defaultOption.value = '';
+          defaultOption.text = 'Selecciona un modelo';
+          modeloSelect.appendChild(defaultOption);
+
           modelos.forEach(modelo => {
               const option = document.createElement('option');
               option.value = modelo.id;
