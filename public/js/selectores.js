fe85ad5480087c3a7fbecbc6bf351cab792c3744
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-23 08:56:11 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -17,16 +17,75 @@ document.addEventListener('DOMContentLoaded', function() {
           // Limpiar el contenedor de productos
           contenedorProductos.innerHTML = '';
 
-          // Añadir los nuevos productos al contenedor
           data.productos.forEach(producto => {
-              const productoDiv = document.createElement('div');
-              productoDiv.classList.add('card');
-
-              // Aquí puedes construir la tarjeta del producto
-              // usando los datos del producto
-
-              contenedorProductos.appendChild(productoDiv);
-          });
+            // Crear elementos para la tarjeta del producto
+            const productoDiv = document.createElement('div');
+            productoDiv.classList.add('card');
+        
+            let imagenes = '';
+            producto.imagenes.forEach((imagen, i) => {
+                imagenes += `<img class="carousel__image ${i !== 0 ? 'hidden' : ''}" src="/uploads/productos/${imagen}" alt="Imagen de ${producto.nombre}">`;
+            });
+        
+            productoDiv.innerHTML = `
+                <div class="cover__card">
+                    <div class="carousel">
+                        ${imagenes}
+                    </div>
+                </div>
+                <div class="carousel__buttons">
+                    <button class="carousel__button">
+                        <i class="fas fa-chevron-left"></i>
+                    </button>
+                    <button class="carousel__button">
+                        <i class="fas fa-chevron-right"></i>
+                    </button>
+                </div>
+                <div class="titulo-producto">
+                    <h3 class="nombre">${producto.nombre}</h3>
+                </div>
+                <hr>
+                <div class="categoria-producto">
+                    <h6 class="categoria">${producto.categoria}</h6>
+                </div>
+                <div class="descripcion" style="display: none;">
+                    ${producto.descripcion}
+                </div>
+                <div class="precio-producto">
+                    <p class="precio">$${typeof producto.precio_venta === 'number' ? Math.floor(producto.precio_venta).toLocaleString('de-DE') : producto.precio_venta}</p>
+                </div>
+                <div class="cantidad-producto">
+                    <a href="/productos/${producto.id}" class="card-link">Ver detalles</a>
+                </div>
+            `;
+        
+            // Agregar funcionalidad a las flechas
+            const carouselImages = productoDiv.querySelectorAll('.carousel__image');
+            const leftButton = productoDiv.querySelector('.carousel__button .fas.fa-chevron-left');
+            const rightButton = productoDiv.querySelector('.carousel__button .fas.fa-chevron-right');
+        
+            let currentImageIndex = 0;
+        
+            leftButton.addEventListener('click', () => {
+                carouselImages[currentImageIndex].classList.add('hidden');
+                currentImageIndex--;
+                if (currentImageIndex < 0) {
+                    currentImageIndex = carouselImages.length - 1;
+                }
+                carouselImages[currentImageIndex].classList.remove('hidden');
+            });
+        
+            rightButton.addEventListener('click', () => {
+                carouselImages[currentImageIndex].classList.add('hidden');
+                currentImageIndex++;
+                if (currentImageIndex >= carouselImages.length) {
+                    currentImageIndex = 0;
+                }
+                carouselImages[currentImageIndex].classList.remove('hidden');
+            });
+        
+            contenedorProductos.appendChild(productoDiv);
+        });
       })
       .catch(error => console.error('Error:', error));
   });
