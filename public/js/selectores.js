7ef19208e39fc1a1a4a98a2bda0cde0583b0ad08
Walter Cordoba <waltercordobadev@gmail.com>
2024-05-23 13:22:55 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -21,6 +21,34 @@ document.getElementById('marca_id').addEventListener('change', function() {
       })
       .catch(error => console.error('Error:', error));
 });
+
+const categoriaSelect = document.getElementById('categoria_id');
+const marcaSelect = document.getElementById('marca_id');
+const modeloSelect = document.getElementById('modelo_id');
+
+marcaSelect.addEventListener('change', function() {
+  const marcaId = this.value;
+  fetch('/productos/modelos/' + marcaId)
+      .then(response => response.json())
+      .then(modelos => {
+          const modeloSelect = document.getElementById('modelo_id');
+          modeloSelect.innerHTML = '';
+
+          // Agrega la opción "Selecciona un modelo"
+          const defaultOption = document.createElement('option');
+          defaultOption.value = '';
+          defaultOption.text = 'Selecciona un modelo';
+          modeloSelect.appendChild(defaultOption);
+
+          modelos.forEach(modelo => {
+              const option = document.createElement('option');
+              option.value = modelo.id;
+              option.text = modelo.nombre;
+              modeloSelect.appendChild(option);
+          });
+      })
+      .catch(error => console.error('Error:', error));
+});
 function actualizarProductos() {
   const categoria = categoriaSelect.value;
   const marca = marcaSelect.value;
