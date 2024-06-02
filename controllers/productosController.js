2acff4f3f71f67d197df2033e23c4f186ef6a148
Walter Cordoba <waltercordobadev@gmail.com>
2024-06-02 15:48:48 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -691,7 +691,7 @@ generarStockPDF: async function (req, res) {
         obtenerProductos.then(productos => {
             console.log('Productos obtenidos:', productos);
             var currentY = doc.y;
-            doc.fontSize(8)
+            doc.fontSize(10)
             .fillColor('blue')
             .text('Código', 70, currentY + 10, {align: 'center', width: 90})
             .text('Descripción', 170, currentY + 10, {align: 'center', width: 250})
@@ -717,7 +717,7 @@ generarStockPDF: async function (req, res) {
                     doc.addPage();
                     currentY = doc.y;
                 }
-                doc.fontSize(10)
+                doc.fontSize(8)
                 .text(producto.codigo_proveedor, 70, currentY + 10, {align: 'center', width: 90})
                 .text(producto.nombre, 170, currentY + 10, {width: 250, continued: true}) // Ajusta el ancho de la descripción
                 .text(producto.stock_minimo ? producto.stock_minimo.toString() : 'N/A', 430, currentY + 10, {width: 45, align: 'center'}) // Ajusta la posición de 'Stock Mínimo'
