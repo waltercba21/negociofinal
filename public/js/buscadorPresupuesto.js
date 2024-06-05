bd2357ce46a3d4514e884e66db106d2bbfa0bf0b
Walter Cordoba <waltercordobadev@gmail.com>
2024-06-05 09:54:33 -0300

    cambios

--- public/js/buscadorPresupuesto.js
+++ public/js/buscadorPresupuesto.js
@@ -28,3 +28,31 @@ document.getElementById('entradaBusqueda').addEventListener('input', async (e) =
     resultadosBusqueda.appendChild(resultado);
   });
 });
+
+/*-------------CALCULAR PRECIO SUBTOTAL------------------*/
+productos.forEach((producto) => {
+  const resultado = document.createElement('div');
+  resultado.textContent = producto.nombre; 
+  resultado.classList.add('resultado-busqueda');
+  resultado.addEventListener('click', () => {
+    const tablaFactura = document.getElementById('tabla-factura').getElementsByTagName('tbody')[0];
+    const filaFactura = tablaFactura.insertRow();
+    const celdaCodigoFactura = filaFactura.insertCell(0);
+    const celdaDescripcionFactura = filaFactura.insertCell(1);
+    const celdaPrecioFactura = filaFactura.insertCell(2);
+    const celdaCantidadFactura = filaFactura.insertCell(3);
+    const celdaSubtotalFactura = filaFactura.insertCell(4); 
+
+    celdaCodigoFactura.textContent = producto.codigo;
+    celdaDescripcionFactura.textContent = producto.nombre; 
+    celdaPrecioFactura.textContent = producto.precio_venta;
+    celdaCantidadFactura.innerHTML = '<input type="number" min="1" value="1">';
+    celdaSubtotalFactura.textContent = producto.precio_venta;
+    resultadosBusqueda.innerHTML = ''; 
+    celdaCantidadFactura.firstChild.addEventListener('input', (e) => {
+      const cantidad = e.target.value;
+      celdaSubtotalFactura.textContent = cantidad * producto.precio_venta;
+    });
+  });
+  resultadosBusqueda.appendChild(resultado);
+});
