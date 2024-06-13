2e1a772235d86e8f660f4bfd7e5444bbe4706522
Walter Cordoba <waltercordobadev@gmail.com>
2024-06-13 08:42:08 -0300

    cambios

--- public/js/panelControl.js
+++ public/js/panelControl.js
@@ -34,45 +34,46 @@ document.getElementById('delete-selected').addEventListener('click', function() 
 
 let timer;
 document.getElementById('entradaBusqueda').addEventListener('input', (e) => {
-    console.log('Evento input detectado');
-    clearTimeout(timer);
-    timer = setTimeout(async () => {
-      const busqueda = e.target.value;
-      console.log('Valor de búsqueda:', busqueda);
-      const contenedorProductos = document.getElementById('contenedor-productos');
-      console.log('Contenedor de productos:', contenedorProductos);
-      contenedorProductos.innerHTML = '';
-      let productos = [];
-      if (!busqueda.trim()) {
-        productos = productosOriginales.slice(0, 12);
-        console.log('Productos originales:', productos);
-      } else {
-        let url = '/productos/api/buscar?q=' + busqueda;
-        console.log('URL de búsqueda:', url);
-        const respuesta = await fetch(url);
-        productos = await respuesta.json();
-        console.log('Productos de la búsqueda:', productos);
-      }
-      productos.forEach((producto, index) => {
-        console.log('Procesando producto', index, producto);
-        const imagen = producto.imagenes && producto.imagenes.length > 0 ? `/uploads/productos/${producto.imagenes[0]}` : '/ruta/valida/a/imagen/por/defecto.jpg';
-        const precio_venta = producto.precio_venta ? `$${Math.floor(producto.precio_venta).toLocaleString('de-DE')}` : 'Precio no disponible';
-        const filaProducto = document.createElement('tr');
-        filaProducto.innerHTML = `
-          <td><input type="checkbox" class="product-check" value="${producto.id}"></td>
-          <td>${producto.categoria}</td>
-          <td>${producto.nombre}</td>
-          <td><img class="img-thumbnail" width='150' src="${imagen}" alt="Imagen de ${producto.nombre}"></td>
-          <td>${precio_venta}</td>
-          <td>
-            <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
-              <form class="form-inline" method="get" action="/productos/editar/${producto.id}?pagina=${paginaActual}">
-                <button class="btn btn-warning" type="submit">Editar</button>
-              </form>
-            </div> 
-          </td>
-        `;
-        contenedorProductos.appendChild(filaProducto);
-      });
-    }, 300);
-  });
+  console.log('Evento input detectado');
+  clearTimeout(timer);
+  timer = setTimeout(async () => {
+    const busqueda = e.target.value;
+    console.log('Valor de búsqueda:', busqueda);
+    const contenedorProductos = document.getElementById('contenedor-productos');
+    console.log('Contenedor de productos:', contenedorProductos);
+    contenedorProductos.innerHTML = '';
+    let productos = [];
+    if (!busqueda.trim()) {
+      productos = productosOriginales.slice(0, 12);
+      console.log('Productos originales:', productos);
+    } else {
+      let url = '/productos/api/buscar?q=' + busqueda;
+      console.log('URL de búsqueda:', url);
+      const respuesta = await fetch(url);
+      const data = await respuesta.json();
+      productos = data.productos; // Aquí es donde se cambió el código
+      console.log('Productos de la búsqueda:', productos);
+    }
+    productos.forEach((producto, index) => {
+      console.log('Procesando producto', index, producto);
+      const imagen = producto.imagenes && producto.imagenes.length > 0 ? `/uploads/productos/${producto.imagenes[0]}` : '/ruta/valida/a/imagen/por/defecto.jpg';
+      const precio_venta = producto.precio_venta ? `$${Math.floor(producto.precio_venta).toLocaleString('de-DE')}` : 'Precio no disponible';
+      const filaProducto = document.createElement('tr');
+      filaProducto.innerHTML = `
+        <td><input type="checkbox" class="product-check" value="${producto.id}"></td>
+        <td>${producto.categoria}</td>
+        <td>${producto.nombre}</td>
+        <td><img class="img-thumbnail" width='150' src="${imagen}" alt="Imagen de ${producto.nombre}"></td>
+        <td>${precio_venta}</td>
+        <td>
+          <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+            <form class="form-inline" method="get" action="/productos/editar/${producto.id}?pagina=${paginaActual}">
+              <button class="btn btn-warning" type="submit">Editar</button>
+            </form>
+          </div> 
+        </td>
+      `;
+      contenedorProductos.appendChild(filaProducto);
+    });
+  }, 300);
+});
