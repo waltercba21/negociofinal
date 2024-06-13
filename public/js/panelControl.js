2a3709bbba6a6245b3af71bcd5389e51d1a5c7de
Walter Cordoba <waltercordobadev@gmail.com>
2024-06-13 09:26:45 -0300

    cambios

--- public/js/panelControl.js
+++ public/js/panelControl.js
@@ -1,35 +1,40 @@
-document.getElementById('check-all').addEventListener('change', function() {
-    var checks = document.querySelectorAll('.product-check');
-    for (var i = 0; i < checks.length; i++) {
-        checks[i].checked = this.checked;
+document.getElementById('contenedor-productos').addEventListener('change', function(event) {
+    if (event.target.matches('.product-check')) {
+        var checks = document.querySelectorAll('.product-check');
+        for (var i = 0; i < checks.length; i++) {
+            checks[i].checked = event.target.checked;
+        }
     }
 });
-document.getElementById('delete-selected').addEventListener('click', function() {
-    var checks = document.querySelectorAll('.product-check');
-    var ids = [];
-    for (var i = 0; i < checks.length; i++) {
-        if (checks[i].checked) {
-            ids.push(checks[i].value);
+
+document.getElementById('contenedor-productos').addEventListener('click', function(event) {
+    if (event.target.matches('#delete-selected')) {
+        var checks = document.querySelectorAll('.product-check');
+        var ids = [];
+        for (var i = 0; i < checks.length; i++) {
+            if (checks[i].checked) {
+                ids.push(checks[i].value);
+            }
         }
+        fetch('/productos/eliminarSeleccionados', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ ids: ids }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                location.reload();
+            } else {
+                console.error('Error al eliminar los productos:', data.error);
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     }
-    fetch('/productos/eliminarSeleccionados', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ ids: ids }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            location.reload();
-        } else {
-            console.error('Error al eliminar los productos:', data.error);
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
 });
 let timer;
 document.getElementById('entradaBusqueda').addEventListener('input', (e) => {
