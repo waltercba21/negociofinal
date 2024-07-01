83599552979be0cfcbc646a4a69c9300f46be00b
Walter Cordoba <waltercordobadev@gmail.com>
2024-07-01 08:29:54 -0300

    cambios

--- public/js/buscadorPresupuesto.js
+++ public/js/buscadorPresupuesto.js
@@ -47,70 +47,69 @@ document.getElementById('invoice-form').addEventListener('submit', async functio
   }
 });
 document.getElementById('entradaBusqueda').addEventListener('input', async (e) => {
-    const busqueda = e.target.value;
-    const resultadosBusqueda = document.getElementById('resultadosBusqueda');
-    resultadosBusqueda.innerHTML = '';
-  
-    if (!busqueda.trim()) {
+  const busqueda = e.target.value;
+  const resultadosBusqueda = document.getElementById('resultadosBusqueda');
+  resultadosBusqueda.innerHTML = '';
+  if (!busqueda.trim()) {
       return;
-    }
-    const url = '/productos/api/buscar?q=' + busqueda;
-    const respuesta = await fetch(url);
-    const productos = await respuesta.json();
-    productos.forEach((producto) => {
+  }
+  const url = '/productos/api/buscar?q=' + busqueda;
+  const respuesta = await fetch(url);
+  const productos = await respuesta.json();
+  const formatter = new Intl.NumberFormat('es-ES', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+  });
+  productos.forEach((producto) => {
       const resultado = document.createElement('div');
       resultado.textContent = producto.nombre;
       resultado.classList.add('resultado-busqueda');
-  
       resultado.addEventListener('click', () => {
-        const tablaFactura = document.getElementById('tabla-factura').getElementsByTagName('tbody')[0];
-        const filaFactura = tablaFactura.insertRow();
-  
-        filaFactura.insertCell(0).textContent = producto.codigo;
-        filaFactura.insertCell(1).textContent = producto.nombre;
-        filaFactura.insertCell(2).textContent = producto.precio_venta;
-  
-        const celdaCantidad = filaFactura.insertCell(3);
-        const inputCantidad = document.createElement('input');
-        inputCantidad.type = 'number';
-        inputCantidad.min = 1;
-        inputCantidad.value = 1;
-        celdaCantidad.appendChild(inputCantidad);
-  
-        filaFactura.insertCell(4).textContent = producto.precio_venta;
-  
-        calcularTotal();
-  
-        inputCantidad.addEventListener('change', () => {
-          const cantidad = parseInt(inputCantidad.value.trim());
-          filaFactura.cells[4].textContent = cantidad * parseFloat(producto.precio_venta);
-          calcularTotal();
-        });
-  
-        resultadosBusqueda.innerHTML = '';
-      });
-  
-      resultadosBusqueda.appendChild(resultado);
-    });
-  });
-  
-  document.addEventListener('click', (e) => {
-    const resultadosBusqueda = document.getElementById('resultadosBusqueda');
-    const entradaBusqueda = document.getElementById('entradaBusqueda');
-  
-    if (!resultadosBusqueda.contains(e.target) && e.target !== entradaBusqueda) {
-      resultadosBusqueda.innerHTML = '';
-    }
-  });
+          const tablaFactura = document.getElementById('tabla-factura').getElementsByTagName('tbody')[0];
+          const filaFactura = tablaFactura.insertRow();
 
-  function calcularTotal() {
-    const filasFactura = document.getElementById('tabla-factura').getElementsByTagName('tbody')[0].rows;
-    let total = 0;
-  
-    for (let i = 0; i < filasFactura.length; i++) {
-      const subtotal = parseFloat(filasFactura[i].cells[4].textContent.trim());
+          filaFactura.insertCell(0).textContent = producto.codigo;
+          filaFactura.insertCell(1).textContent = producto.nombre;
+          filaFactura.insertCell(2).textContent = formatter.format(producto.precio_venta);
+
+          const celdaCantidad = filaFactura.insertCell(3);
+          const inputCantidad = document.createElement('input');
+          inputCantidad.type = 'number';
+          inputCantidad.min = 1;
+          inputCantidad.value = 1;
+          celdaCantidad.appendChild(inputCantidad);
+
+          filaFactura.insertCell(4).textContent = formatter.format(producto.precio_venta);
+
+          calcularTotal();
+
+          inputCantidad.addEventListener('change', () => {
+              const cantidad = parseInt(inputCantidad.value.trim());
+              filaFactura.cells[4].textContent = formatter.format(cantidad * parseFloat(producto.precio_venta.replace(/\D/g, '')));
+              calcularTotal();
+          });
+
+          resultadosBusqueda.innerHTML = '';
+      });
+
+      resultadosBusqueda.appendChild(resultado);
+  });
+});
+
+function calcularTotal() {
+  const filasFactura = document.getElementById('tabla-factura').getElementsByTagName('tbody')[0].rows;
+  let total = 0;
+  const formatter = new Intl.NumberFormat('es-ES', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+  });
+  for (let i = 0; i < filasFactura.length; i++) {
+      const subtotal = parseFloat(filasFactura[i].cells[4].textContent.replace(/\D/g, ''));
       total += subtotal;
-    }
-    document.getElementById('total-amount').value = total.toFixed(2);
   }
-  
+  document.getElementById('total-amount').value = formatter.format(total);
+}
