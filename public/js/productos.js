2d1a853d8345c888cc5a069fa07533435b92f06b
Walter Cordoba <waltercordobadev@gmail.com>
2024-07-24 19:43:35 -0300

    cambios

--- public/js/productos.js
+++ public/js/productos.js
@@ -1,27 +1,23 @@
 $(document).ready(function() {
-  $('.carousel').each(function() {
-    var $carousel = $(this);
-    var $images = $carousel.find('.carousel__image');
-    var $prevButton = $carousel.closest('.card').find('.carousel__button:has(.fa-chevron-left)');
-    var $nextButton = $carousel.closest('.card').find('.carousel__button:has(.fa-chevron-right)');
-    var index = 0;
+  var $images = $('.carousel__image');
+  var index = 0;
 
-    // Ocultar todas las imágenes excepto la primera
-    $images.hide().eq(index).show();
+  function showImage(newIndex) {
+    $images.removeClass('active').hide();
+    $images.eq(newIndex).addClass('active').show();
+    index = newIndex;
+  }
 
-    $prevButton.on('click', function() {
-      console.log('Prev button clicked'); // Añade una traza para verificar el clic
-      $images.eq(index).hide();
-      index = (index > 0) ? index - 1 : $images.length - 1;
-      $images.eq(index).show();
-    });
+  $('#prevButton').on('click', function() {
+    console.log('Prev button clicked'); // Añade una traza para verificar el clic
+    var newIndex = (index > 0) ? index - 1 : $images.length - 1;
+    showImage(newIndex);
+  });
 
-    $nextButton.on('click', function() {
-      console.log('Next button clicked'); // Añade una traza para verificar el clic
-      $images.eq(index).hide();
-      index = (index < $images.length - 1) ? index + 1 : 0;
-      $images.eq(index).show();
-    });
+  $('#nextButton').on('click', function() {
+    console.log('Next button clicked'); // Añade una traza para verificar el clic
+    var newIndex = (index < $images.length - 1) ? index + 1 : 0;
+    showImage(newIndex);
   });
 });
 
