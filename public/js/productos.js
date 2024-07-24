3ab2c61c6d7381978bee51304e797849c892e863
Walter Cordoba <waltercordobadev@gmail.com>
2024-07-24 19:36:12 -0300

    cambios

--- public/js/productos.js
+++ public/js/productos.js
@@ -10,12 +10,14 @@ $(document).ready(function() {
     $images.hide().eq(index).show();
 
     $prevButton.on('click', function() {
+      console.log('Prev button clicked'); // Añade una traza para verificar el clic
       $images.eq(index).hide();
       index = (index > 0) ? index - 1 : $images.length - 1;
       $images.eq(index).show();
     });
 
     $nextButton.on('click', function() {
+      console.log('Next button clicked'); // Añade una traza para verificar el clic
       $images.eq(index).hide();
       index = (index < $images.length - 1) ? index + 1 : 0;
       $images.eq(index).show();
