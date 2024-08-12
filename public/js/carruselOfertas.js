9fcad490b5d1515fee4f7b27efad2b109b656874
Walter Cordoba <waltercordobadev@gmail.com>
2024-08-11 21:22:10 -0300

    cambios

--- public/js/carruselOfertas.js
+++ public/js/carruselOfertas.js
@@ -4,12 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const flechaDerecha = document.querySelector('.flecha-derecha');
   
     let scrollAmount = 0;
-    const cardWidth = 150; // Ancho de cada tarjeta más el margen
+    const cardWidth = 150; // Ancho de cada tarjeta (sin incluir márgenes)
+    const visibleCards = 3; // Número de tarjetas visibles a la vez
   
     flechaDerecha.addEventListener('click', function() {
       const maxScroll = contenedorProductos.scrollWidth - contenedorProductos.clientWidth;
       if (scrollAmount < maxScroll) {
-        scrollAmount += cardWidth * 3; // Mueve 3 tarjetas a la vez
+        scrollAmount += cardWidth * visibleCards; // Mueve 3 tarjetas a la vez
         if (scrollAmount > maxScroll) scrollAmount = maxScroll;
         contenedorProductos.scrollTo({
           left: scrollAmount,
@@ -20,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
     flechaIzquierda.addEventListener('click', function() {
       if (scrollAmount > 0) {
-        scrollAmount -= cardWidth * 3; // Mueve 3 tarjetas a la vez
+        scrollAmount -= cardWidth * visibleCards; // Mueve 3 tarjetas a la vez
         if (scrollAmount < 0) scrollAmount = 0;
         contenedorProductos.scrollTo({
           left: scrollAmount,
