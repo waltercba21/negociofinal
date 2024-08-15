afaa2235068cd38792e39229425083320f4ffdc2
Walter Cordoba <waltercordobadev@gmail.com>
2024-08-15 11:49:08 -0300

    cambios

--- public/js/editar.js
+++ public/js/editar.js
@@ -56,6 +56,9 @@ $(document).ready(function() {
         $('#modelo_id').empty();
         $('#modelo_id').append('<option value="">Selecciona un modelo...</option>');
         $.get('/productos/modelos/' + marcaId, function(modelosPorMarca) {
+            modelosPorMarca.sort(function(a, b) {
+                return a.id - b.id;
+            });
             modelosPorMarca.forEach(function(modelo) {
                 $('#modelo_id').append('<option value="' + modelo.id + '">' + modelo.nombre + '</option>');
             });
@@ -67,6 +70,9 @@ $(document).ready(function() {
         $('#modelo_id').empty();
         $('#modelo_id').append('<option value="">Selecciona un modelo...</option>');
         $.get('/productos/modelos/' + marcaId, function(modelosPorMarca) {
+            modelosPorMarca.sort(function(a, b) {
+                return a.id - b.id;
+            });
             modelosPorMarca.forEach(function(modelo) {
                 $('#modelo_id').append('<option value="' + modelo.id + '">' + modelo.nombre + '</option>');
             });
