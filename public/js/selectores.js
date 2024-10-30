cd9d6b126dda35461933fc34071b14d5e9e791fd
Walter Cordoba <waltercordobadev@gmail.com>
2024-10-30 18:36:21 -0300

    cambios

--- public/js/selectores.js
+++ public/js/selectores.js
@@ -31,112 +31,114 @@ document.addEventListener('DOMContentLoaded', function() {
 
   categoriaSelect.addEventListener('change', realizarBusqueda);
   marcaSelect.addEventListener('change', function() {
-    const marcaId = this.value;
+      const marcaId = this.value;
 
-    // Cargar modelos relacionados a la marca seleccionada
-    if (marcaId) {
-      fetch(`/productos/modelos/${marcaId}`)
-        .then(response => response.json())
-        .then(modelos => {
-          modelos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+      // Cargar modelos relacionados a la marca seleccionada
+      if (marcaId) {
+          fetch(`/productos/modelos/${marcaId}`)
+              .then(response => response.json())
+              .then(modelos => {
+                  modelos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+                  modeloSelect.innerHTML = '<option value="">Selecciona un modelo</option>';
+                  modelos.forEach(modelo => {
+                      const option = document.createElement('option');
+                      option.value = modelo.id;
+                      option.text = modelo.nombre;
+                      modeloSelect.appendChild(option);
+                  });
+                  realizarBusqueda(); // Ejecuta la búsqueda en caso de que ya haya datos seleccionados
+              })
+              .catch(error => console.error('Error:', error));
+      } else {
           modeloSelect.innerHTML = '<option value="">Selecciona un modelo</option>';
-          modelos.forEach(modelo => {
-            const option = document.createElement('option');
-            option.value = modelo.id;
-            option.text = modelo.nombre;
-            modeloSelect.appendChild(option);
-          });
-          realizarBusqueda(); // Ejecuta la búsqueda en caso de que ya haya datos seleccionados
-        })
-        .catch(error => console.error('Error:', error));
-    } else {
-      modeloSelect.innerHTML = '<option value="">Selecciona un modelo</option>';
-    }
+          realizarBusqueda(); // Realiza la búsqueda si no hay marca seleccionada
+      }
   });
+
   modeloSelect.addEventListener('change', realizarBusqueda);
 
   function realizarBusqueda() {
-    const categoria_id = categoriaSelect.value;
-    const marca_id = marcaSelect.value;
-    const modelo_id = modeloSelect.value;
+      const categoria_id = categoriaSelect.value;
+      const marca_id = marcaSelect.value;
+      const modelo_id = modeloSelect.value;
 
-    // Construir la URL con los parámetros y la paginación
-    let url = `/productos/api/buscar?limite=${productosPorPagina}&pagina=${paginaActual}&`;
-    if (categoria_id) url += `categoria_id=${categoria_id}&`;
-    if (marca_id) url += `marca_id=${marca_id}&`;
-    if (modelo_id) url += `modelo_id=${modelo_id}`;
-    url = url.replace(/&$/, ''); // Remueve '&' extra al final de la URL
+      // Construir la URL con los parámetros y la paginación
+      let url = `/productos/api/buscar?limite=${productosPorPagina}&pagina=${paginaActual}&`;
+      if (categoria_id) url += `categoria_id=${categoria_id}&`;
+      if (marca_id) url += `marca_id=${marca_id}&`;
+      if (modelo_id) url += `modelo_id=${modelo_id}`;
+      url = url.replace(/&$/, ''); // Remueve '&' extra al final de la URL
 
-    if (categoria_id || marca_id || modelo_id) {
-      fetch(url)
-        .then(response => {
-          if (!response.ok) throw new Error('Error en la red');
-          return response.json();
-        })
-        .then(data => {
-          const productos = data.productos || [];
-          if (productos.length === 0) {
-            contenedorProductos.innerHTML = '<p>No se encontraron productos. Refinar la búsqueda.</p>';
-          } else {
-            renderizarProductos(productos, contenedorProductos, false);
-            if (productos.length < productosPorPagina) {
-              contenedorProductos.innerHTML += '<p>Mostrando todos los resultados disponibles.</p>';
-            }
-          }
-        })
-        .catch(error => console.error('Error:', error));
-    } else {
-      contenedorProductos.innerHTML = "<p>Seleccione al menos una categoría, marca o modelo para realizar la búsqueda.</p>";
-    }
+      if (categoria_id || marca_id || modelo_id) {
+          fetch(url)
+              .then(response => {
+                  if (!response.ok) throw new Error('Error en la red');
+                  return response.json();
+              })
+              .then(data => {
+                  const productos = data.productos || [];
+                  if (productos.length === 0) {
+                      contenedorProductos.innerHTML = '<p>No se encontraron productos. Refinar la búsqueda.</p>';
+                  } else {
+                      renderizarProductos(productos, contenedorProductos, false);
+                      if (productos.length < productosPorPagina) {
+                          contenedorProductos.innerHTML += '<p>Mostrando todos los resultados disponibles.</p>';
+                      }
+                  }
+              })
+              .catch(error => console.error('Error:', error));
+      } else {
+          contenedorProductos.innerHTML = "<p>Seleccione al menos una categoría, marca o modelo para realizar la búsqueda.</p>";
+      }
   }
 
   function renderizarProductos(productos, contenedorProductos, isAdminUser) {
-    contenedorProductos.innerHTML = '';
-    productos.forEach(producto => {
-      const imagenes = producto.imagenes?.map((img, i) => `
-        <img class="carousel__image ${i !== 0 ? 'hidden' : ''}" src="/uploads/productos/${img.imagen}" alt="Imagen de ${producto.nombre}">
-      `).join('') || '<img src="/ruta/a/imagen/defecto.jpg" alt="Imagen no disponible">';
+      contenedorProductos.innerHTML = '';
+      productos.forEach(producto => {
+          const imagenes = producto.imagenes?.map((img, i) => `
+              <img class="carousel__image ${i !== 0 ? 'hidden' : ''}" src="/uploads/productos/${img.imagen}" alt="Imagen de ${producto.nombre}">
+          `).join('') || '<img src="/ruta/a/imagen/defecto.jpg" alt="Imagen no disponible">';
 
-      const precioVenta = producto.precio_venta ? `$${Math.floor(producto.precio_venta).toLocaleString('de-DE')}` : 'Precio no disponible';
+          const precioVenta = producto.precio_venta ? `$${Math.floor(producto.precio_venta).toLocaleString('de-DE')}` : 'Precio no disponible';
 
-      let tarjetaProducto = `
-        <div class="card">
-          <div class="carousel">
-            ${imagenes}
-          </div>
-          <div class="titulo-producto"><h3 class="nombre">${producto.nombre}</h3></div>
-          <hr>
-          <div class="precio-producto"><p class="precio">${precioVenta}</p></div>
-          <a href="/productos/${producto.id}" class="card-link">Ver detalles</a>
-        </div>
-      `;
+          let tarjetaProducto = `
+              <div class="card">
+                  <div class="carousel">
+                      ${imagenes}
+                  </div>
+                  <div class="titulo-producto"><h3 class="nombre">${producto.nombre}</h3></div>
+                  <hr>
+                  <div class="precio-producto"><p class="precio">${precioVenta}</p></div>
+                  <a href="/productos/${producto.id}" class="card-link">Ver detalles</a>
+              </div>
+          `;
 
-      if (isAdminUser) {
-        tarjetaProducto += `
-          <div class="stock-producto ${producto.stock_actual < producto.stock_minimo ? 'bajo-stock' : 'suficiente-stock'}">
-            <p>Stock Disponible: ${producto.stock_actual}</p>
-          </div>
-        `;
-      }
+          if (isAdminUser) {
+              tarjetaProducto += `
+                  <div class="stock-producto ${producto.stock_actual < producto.stock_minimo ? 'bajo-stock' : 'suficiente-stock'}">
+                      <p>Stock Disponible: ${producto.stock_actual}</p>
+                  </div>
+              `;
+          }
 
-      contenedorProductos.innerHTML += tarjetaProducto;
-    });
+          contenedorProductos.innerHTML += tarjetaProducto;
+      });
   }
 
   // Control de carrusel
   $(document).on('click', '.carousel__button', function() {
-    const $carousel = $(this).closest('.card').find('.carousel');
-    const $images = $carousel.find('.carousel__image');
-    let index = $images.index($carousel.find('.carousel__image:visible'));
+      const $carousel = $(this).closest('.card').find('.carousel');
+      const $images = $carousel.find('.carousel__image');
+      let index = $images.index($carousel.find('.carousel__image:visible'));
 
-    if ($(this).find('.fa-chevron-left').length > 0) {
-      $images.eq(index).hide();
-      index = (index - 1 + $images.length) % $images.length;
-    } else {
-      $images.eq(index).hide();
-      index = (index + 1) % $images.length;
-    }
+      if ($(this).find('.fa-chevron-left').length > 0) {
+          $images.eq(index).hide();
+          index = (index - 1 + $images.length) % $images.length;
+      } else {
+          $images.eq(index).hide();
+          index = (index + 1) % $images.length;
+      }
 
-    $images.eq(index).show();
+      $images.eq(index).show();
   });
 });
