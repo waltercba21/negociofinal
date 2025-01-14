ced1101a63cbcac0abade6316e27859071845b1d
Walter Cordoba <waltercordobadev@gmail.com>
2025-01-14 12:41:05 -0300

    cambios

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,12 +1,8 @@
-// Array de correos electrónicos de administradores
-const adminEmails = ['walter@autofaros.com.ar', 'chacho@autofaros.com.ar', 'gera@autofaros.com.ar'];
+// middleware/adminMiddleware.js
 
 module.exports = (req, res, next) => {
   // Verificar si el usuario es el administrador
-  const isAdminUser = (
-    req.session.usuario && adminEmails.includes(req.session.usuario.email) ||
-    req.session.isAdminUser // Nueva condición para manejar al admin incluso si no ha iniciado sesión
-  );
+  const isAdminUser = req.session.usuario && req.session.usuario.isAdmin;
 
   // Agregar una propiedad al objeto res.locals para usar en las vistas
   res.locals.isAdminUser = isAdminUser;
