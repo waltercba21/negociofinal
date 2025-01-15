843aaae0e4ab7e56b16804d8f16be9f35d72d4b1
Walter Cordoba <waltercordobadev@gmail.com>
2025-01-14 22:18:11 -0300

    cambios

--- config/conexion.js
+++ config/conexion.js
@@ -1,23 +1,26 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1', 
-    port:'3306',
-    user: 'walter',
-    password: '123456',
-    database: 'autofaros',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'walter',
+  password: '123456',
+  database: 'autofaros',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-pool.getConnection((error, connection) => {
-    if (error) {
-        console.error('Error de conexion:', error);
-    } else {
-        console.log('Conexion establecida a la base de datos');
-        connection.release();
-    }
-});
+const getConexion = () => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((error, connection) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+};
 
-module.exports = pool;
+module.exports = { getConexion, pool };
