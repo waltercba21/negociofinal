fc967ce81c885f039272d6043be68c6cdb213260
Walter Cordoba <waltercordobadev@gmail.com>
2025-02-19 21:39:13 -0300

    cambios

--- routes/carrito.js
+++ routes/carrito.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const carritoController = require('../controllers/carritoController');
-const calcularCantidadCarrito = require('../middleware/carritoMiddleware');
-
-// Aplica el middleware solo a las rutas relacionadas con el carrito
-router.use(calcularCantidadCarrito);
 
 // Ruta para crear un carrito (si no existe uno activo)
 router.get('/crear', carritoController.crearCarrito);
