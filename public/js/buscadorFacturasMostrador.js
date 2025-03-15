cf41e77c573f2bf6e2a378d34a195e9084de82d4
Walter Cordoba <waltercordobadev@gmail.com>
2025-03-15 09:41:23 -0300

    cambios

--- public/js/buscadorFacturasMostrador.js
+++ public/js/buscadorFacturasMostrador.js
@@ -55,13 +55,6 @@ document.getElementById('invoice-form').addEventListener('submit', async functio
         });
         return;
     }
-        // 🔥 Establecer la fecha actual en el input de fecha
-        const fechaPresupuestoInput = document.getElementById('fecha-presupuesto');
-        if (fechaPresupuestoInput) {
-            const today = new Date();
-            const formattedDate = today.toISOString().split('T')[0];
-            fechaPresupuestoInput.value = formattedDate;
-        }
     
     const totalFacturaElement = document.getElementById('total-amount');
     let totalFactura = '0';
@@ -130,7 +123,14 @@ document.addEventListener('DOMContentLoaded', () => {
         icon: 'info',
         confirmButtonText: 'Entendido'
     });
-
+            // 🔥 Establecer la fecha actual en el input de fecha
+            const fechaPresupuestoInput = document.getElementById('fecha-presupuesto');
+            if (fechaPresupuestoInput) {
+                const today = new Date();
+                const formattedDate = today.toISOString().split('T')[0];
+                fechaPresupuestoInput.value = formattedDate;
+            }
+        
     const entradaBusqueda = document.getElementById('entradaBusqueda');
     const resultadosBusqueda = document.getElementById('resultadosBusqueda');
     let timeoutId;
