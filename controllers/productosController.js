50004bfe7892506621adffdd66d467f74d771764
Walter Cordoba <waltercordobadev@gmail.com>
2025-04-17 09:07:07 -0300

    cambios

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -417,7 +417,7 @@ module.exports = {
             estado: req.body.estado,
             stock_minimo: req.body.stock_minimo,
             stock_actual: req.body.stock_actual,
-            oferta: req.body.oferta ? 1 : 0,
+            oferta: Number(req.body.oferta) === 1 ? 1 : 0,
             calidad_original: req.body.calidad_original_fitam ? 1 : 0,
             calidad_vic: req.body.calidad_vic ? 1 : 0,
         };
