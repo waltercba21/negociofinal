1ab95820aeb57f0bdc8050387f69ec8018ce16dd
Walter Cordoba <waltercordobadev@gmail.com>
2025-04-18 13:13:04 -0300

    cambios

--- models/pedidos.js
+++ models/pedidos.js
@@ -5,6 +5,7 @@ module.exports = {
         let query = `
             SELECT c.id AS id_pedido, u.nombre AS cliente, c.estado, c.tipo_envio, 
                    COALESCE(SUM(pc.cantidad * p.precio_venta), 0) AS total
+                   c.creado_en AS fecha
             FROM carritos c
             JOIN usuarios u ON c.usuario_id = u.id
             LEFT JOIN productos_carrito pc ON c.id = pc.carrito_id
