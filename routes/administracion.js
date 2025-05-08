60821d5e13ca7c04ef0eae841cea998cb579e0cd
Walter Cordoba <waltercordobadev@gmail.com>
2025-05-08 15:18:32 -0300

    cambios

--- routes/administracion.js
+++ routes/administracion.js
@@ -26,7 +26,8 @@ router.get('/facturas/eliminar/:id', administracionController.getEliminarFactura
 router.post('/facturas/eliminar/:id', administracionController.postEliminarFactura);
 router.get('/facturas/proveedor', administracionController.generarPDFProveedor);
 
-router.post('/api/facturas', administracionController.apiFacturas);
+router.post('/api/facturas', administracionController.postFactura);
+
 router.post('/api/factura/productos', administracionController.guardarItemsFactura);
 
 
