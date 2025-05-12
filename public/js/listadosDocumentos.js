e8d35992e849bfa935ef1c85446b6f206546eb0b
Walter Cordoba <waltercordobadev@gmail.com>
2025-05-12 13:02:21 -0300

    cambios

--- public/js/listadosDocumentos.js
+++ public/js/listadosDocumentos.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let datosFiltrados = [];
   let paginaActual = 1;
-  const tarjetasPorPagina = 10;
+  const tarjetasPorPagina = 4;
 
   btnBuscar.addEventListener('click', async () => {
     const proveedor = document.getElementById('filtroProveedor').value;
@@ -61,9 +61,9 @@ document.addEventListener('DOMContentLoaded', () => {
   <div class="card resultado-doc shadow-sm">
     <div class="card-body">
       <h6>${doc.tipo.toUpperCase()}</h6>
+      <p><strong>Proveedor:</strong> ${doc.nombre_proveedor}</p>
       <p><strong>Número:</strong> ${doc.numero}</p>
       <p><strong>Fecha:</strong> ${new Date(doc.fecha).toLocaleDateString()}</p>
-      <p><strong>Proveedor:</strong> ${doc.nombre_proveedor}</p>
       <button class="btn btn-outline-primary btn-sm ver-mas-documento" data-id="${doc.id}" data-tipo="${doc.tipo}">
         Ver más
       </button>
