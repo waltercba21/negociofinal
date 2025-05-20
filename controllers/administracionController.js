edb7b0a2590279ac524e38acc5ceedca87ff6acc
Walter Cordoba <waltercordobadev@gmail.com>
2025-05-20 09:27:25 -0300

    cambios

--- controllers/administracionController.js
+++ controllers/administracionController.js
@@ -2,6 +2,14 @@ const administracion = require('../models/administracion')
 const path = require('path');
 const PDFDocument = require('pdfkit');
 
+function formatFechaDMY(date) {
+  const d = new Date(date);
+  const day = String(d.getDate()).padStart(2, '0');
+  const month = String(d.getMonth() + 1).padStart(2, '0');
+  const year = d.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
 
 function formatDate(date) {
     var d = new Date(date),
