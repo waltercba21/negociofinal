08504b4843b2d3e4c6a9d54930671da7da604274
Walter Cordoba <waltercordobadev@gmail.com>
2026-01-28 08:21:09 -0300

    cambios

--- models/producto.js
+++ models/producto.js
@@ -2313,8 +2313,6 @@ obtenerProveedoresPorProducto: async (conexion, producto_id) => {
   const [rows] = await conexion.promise().query(sql, [pedidoId]);
   return rows?.[0] || null;
 },
-
-// Ajustá el nombre de tabla si no es "pedidos_items"
 obtenerItemsPedido: async function (conexion, pedidoId, proveedorId) {
   const sql = `
     SELECT
@@ -2323,7 +2321,7 @@ obtenerItemsPedido: async function (conexion, pedidoId, proveedorId) {
       pi.cantidad,
       pp.codigo,
       pp.costo_neto
-    FROM pedidos_items pi
+    FROM pedido_items pi
     JOIN productos p ON p.id = pi.producto_id
     LEFT JOIN producto_proveedor pp
       ON pp.producto_id = pi.producto_id
@@ -2356,13 +2354,13 @@ upsertPedido: async function (conexion, { pedido_id, proveedor_id, total, produc
       );
 
       // reemplazo items
-      await cx.query(`DELETE FROM pedidos_items WHERE pedido_id = ?`, [pedidoId]);
+      await cx.query(`DELETE FROM pedido_items WHERE pedido_id = ?`, [pedidoId]);
     }
 
     // insertar items
     const values = productos.map(p => [pedidoId, Number(p.id), Number(p.cantidad) || 1]);
     await cx.query(
-      `INSERT INTO pedidos_items (pedido_id, producto_id, cantidad) VALUES ?`,
+      `INSERT INTO pedido_items (pedido_id, producto_id, cantidad) VALUES ?`,
       [values]
     );
 
