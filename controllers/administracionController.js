499118ddd3434112c7f01c1dd2b5231a6f39d2e8
Walter Cordoba <waltercordobadev@gmail.com>
2026-03-07 19:03:09 -0300

    cambios

--- controllers/administracionController.js
+++ controllers/administracionController.js
@@ -1,16 +1,18 @@
-const administracion = require('../models/administracion')
+// ✅ Después
+const administracion = require('../models/administracion');
 const path = require('path');
 const PDFDocument = require('pdfkit');
-
-function formatFechaDMY(date) {
-  const d = new Date(date);
-  const day = String(d.getDate()).padStart(2, '0');
-  const month = String(d.getMonth() + 1).padStart(2, '0');
-  const year = d.getFullYear();
-  return `${day}/${month}/${year}`;
-}
-
-
+const pdfmake = require('pdfmake/build/pdfmake');
+const pdfFonts = require('pdfmake/build/vfs_fonts');
+pdfmake.vfs = pdfFonts.pdfMake.vfs;
+const fonts = {
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+    bold: 'Roboto-Medium.ttf',
+    italics: 'Roboto-Italic.ttf',
+    bolditalics: 'Roboto-MediumItalic.ttf'
+  }
+};
 function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
@@ -264,7 +266,6 @@ postFactura: function (req, res) {
         });
     },    
     generarPDFProveedor : function(req, res) {
-        var printer = new pdfmake(fonts);
         var idProveedor = req.query.proveedorListado;
     
         // Obtén los detalles del proveedor
