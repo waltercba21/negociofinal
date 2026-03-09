497e228a8b6a0bd145a686d671f9a1866d8a4a6a
Walter Cordoba <waltercordobadev@gmail.com>
2026-03-09 10:59:52 -0300

    cambios

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -3,7 +3,7 @@ const adminEmails = require('../config/admins');
 module.exports = {
   setGlobalVariables: (req, res, next) => {
     res.locals.isLogged = !!req.session.usuario;
-    res.locals.isAdminUser = req.session.usuario && adminEmails.includes(req.session.usuario.email);
+    res.locals.isAdminUser = !!(req.session.usuario && adminEmails.includes(req.session.usuario.email));
     next();
   }
 };
